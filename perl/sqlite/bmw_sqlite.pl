ffa8f0a1a02ea89616acead0e5bf4556370770b4
Radu Moisa <rmoisa@yahoo.com>
2010-10-21 11:47:50 +0400

    the working one

--- perl/sqlite/bmw_sqlite.pl
+++ perl/sqlite/bmw_sqlite.pl
@@ -1,7 +1,8 @@
-#!/usr/bin/perl
+#!/PROJ/cis/gen/general/sun_utils/gnu/sun4/bin.solaris/perl
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 use DBI;
 
@@ -21,6 +22,8 @@ sub query{
 	}
 	else{
 		my $rows_found=$#$array_ref+1;
+		$query=~/(\w+)\s(.+)/;
+#		print "$1 returned $rows_found rows.\n";
 		if ($rows_found>0){
 #			for my $arr ($array_ref){
 #				for my $a (@$arr){
@@ -30,13 +33,15 @@ sub query{
 #					print "\n";
 #				}
 #			}
-#			print "Returned $rows_found rows.\n";
 			return @$array_ref;
+		}
+		else{
+			return undef;
 		}
 	}
 }
 
-my $basedir="./";
+my $basedir="/HOME/ccm_wet/local/scripts/sqlite";
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=".$basedir."/bmw_ids.db","","");
 &query($dbh,"select sqlite_version()");
@@ -45,16 +50,19 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=".$basedir."/bmw_ids.db","","");
 
 &query($dbh,"create table bmw_ids(id INTEGER PRIMARY KEY,conti_id TEXT,bmw_id INTEGER)");
 
-&query($dbh,"select * from bmw_ids");
+#&query($dbh,"select * from bmw_ids");
 
 open FILE,"$basedir/bmw_ids.txt";
 
+my $cnt=0;
 while (my $line=<FILE>){
+	if ($cnt>1000){
+		last;
+	}
 	chomp $line;
 	my @l=split(/,/,$line);
 	my @found_ids=&query($dbh,"select * from bmw_ids where bmw_id='".$l[1]."'");
-#	print "Cu asta comparam: ".$#found_ids."\n";
-	if ($#found_ids>=0){
+	if ($found_ids[0]){
 		print "Updating $l[0] -> $l[1]\n";
 		&query($dbh,"update bmw_ids set conti_id='".$l[0]."' where bmw_id='".$l[1]."'");
 	}
@@ -62,10 +70,11 @@ while (my $line=<FILE>){
 		print "Inserting $l[0] -> $l[1]\n";
 		&query($dbh,"insert into bmw_ids(conti_id,bmw_id) values ('".$l[0]."',".$l[1].")");
 	}
+	$cnt++;
 }
 close(FILE);
 
-&query($dbh,"select * from bmw_ids");
+#&query($dbh,"select * from bmw_ids");
 
 $dbh->disconnect;
 
