21f29f2ff749e555f26226234252ffa371c41422
Radu.Moisa <rmoisa@yahoo.com>
2010-11-17 21:21:31 +0200

    added safety checks

--- perl/work/ftpfunc_new.pl
+++ perl/work/ftpfunc_new.pl
@@ -8,9 +8,21 @@ use Net::FTP;
 #returns connection to server
 sub start_conn{
 	my ($server,$user,$pass)=@_;
-	my $ftp = Net::FTP->new($server, Debug => 0, Passive=>0, Firewall=>'proxy.rb.de.conti.de', FirewallType=>1);
-	$ftp->login($user,$pass) or die $ftp->message;
-	return $ftp;
+#	my $ftp = Net::FTP->new($server, Debug => 0, Passive=>0, Firewall=>'proxy.rb.de.conti.de', FirewallType=>1);
+	my $ftp = Net::FTP->new($server, Timeout => 600, BlockSize => 1024) or die "Could not connect to server!";
+	my $login;
+	eval{
+		$login=$ftp->login($user,$pass);
+		print "Login: $login\n";
+	};
+	print $ftp->message();
+	if ($login ne "1" or $@){
+		print "Could not start FTP session\n";
+		return undef;
+	}
+	else{
+		return $ftp;
+	}
 }
 
 sub upload_file{
@@ -20,21 +32,36 @@ sub upload_file{
                 my $ok=0;
                 my $try=0;
                 my $conn;
+		$|=1;
                 while ($ok==0 and $try<$max_tries){
-                        $conn=&start_conn("$server","$user","$pass");
-                        $conn->cwd($dir) or die "Could not change to remote directory $dir !\n";
-                        $conn->binary();
-                        my $rez=$conn->put("$file");
-                        if ($rez and $rez eq $file){
-                                print "Successfully uploaded file $file on try #$try\n";
-                                $ok=1;
-                        }
-                        else{
-                                print "Fail #$try uploading $file\n";
-                                $try++;
-                        }
+			$conn=&start_conn("$server","$user","$pass");
+			if ($conn){
+	                        $conn->cwd($dir) or die "Could not change to remote directory $dir !\n";
+        	                $conn->binary();
+				my $rez;
+				eval{
+					print "Uploading $file...\n";
+               	         		$rez=$conn->put("$file");
+				};
+				if ($@){
+					print "Major failure uploading $file!\n";
+					$try++;
+				}
+	                        if ($rez and $rez eq $file){
+       	                        	print "Successfully uploaded file $file on try #$try\n";
+       	                        	$ok=1;
+	                        }
+	                        else{
+	                                print "Fail #$try uploading $file\n";
+	                                $try++;
+	                        }
+			}
+			else{
+				exit;
+			}	
                 }
                 &stop_conn($conn);
+		$|=0;
                 if ($ok==0){
                         print "Could not upload $file after $try tries!\n";
                         exit 1;
---
+++ perl/work/test_ftp.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+do "ftpfunc_new.pl";
+
+chdir "/home/slash";
+
+&upload_file("localhost","slash","nokia13","/tmp","1000MB");
+
