6616eb26b36814713363ccc048770a5f5b8bceff
Radu Moisa <rmoisa@yahoo.com>
2010-11-28 14:04:36 +0300

    sorting, etc

--- ruby/rand_mem.rb
+++ ruby/rand_mem.rb
@@ -1,13 +1,26 @@
 #!/usr/bin/ruby
 
-max=99999999
-no=1000
+max=10
+no=100
+
 hash=Hash.new
-puts "Adding "+no.to_s+" values..."
 (0..no).each do |i|
 	val=rand(max)
 	hash[i]=val
-	if (i % 1000 ==0) then
-		puts i.to_s+": "+val.to_s
-	end
 end
+
+array=Array.new
+array=hash.sort {|a,b| b[1]<=>a[1]}
+
+count_hash=Hash.new(0)
+array.each do |pair|
+	count_hash[pair[1]]+=1
+	puts pair[0].to_s+' -> '+pair[1].to_s
+end
+
+count_array=count_hash.sort
+puts 'Counted Values:'
+count_array.each do |pair|
+	puts pair[0].to_s+' -> '+pair[1].to_s
+end
+
