0faf895116d7c326bb65ffdd892bf1a987d3a009
Radu Moisa <rmoisa@yahoo.com>
2011-01-12 13:02:13 +0200

    mem safe try

---
+++ perl/vdir_memsafe.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::stat;
+use File::Find;
+use POSIX qw(strftime);
+use Switch;
+
+use Data::Dumper;
+
+my %files_by_modify_time;
+my %files_by_size;
+
+sub check_files{
+	my $file=$_;
+	my $dir=$File::Find::dir;
+	chdir $dir;
+	if (-f $_){
+		$files_by_modify_time{"$dir/$file"}=(stat($_))->[9];
+		$files_by_size{"$dir/$file"}=(stat($_))->[7];
+	}
+	undef $file;
+	undef $dir;
+}
+
+sub sort_by_modify_time{
+	my (%files)=%{$_[0]};
+	for (sort { $files{$a} <=> $files{$b} } keys %files){
+       		print strftime("%Y/%m/%d %H:%M:%S",localtime($files{$_}))."\t$_\n";
+	}
+	%files=();
+	undef %files;
+}
+
+sub sort_by_size{
+        my (%files)=%{$_[0]};
+        for (sort { $files{$a} <=> $files{$b} } keys %files){
+                print $files{$_}."\t$_\n";
+        }
+	%files=();
+        undef %files;
+}
+
+if ($#ARGV<1){
+        print "Usage: ./vdir.pl <time|size> <dir path>\n";
+	exit;
+}
+
+my $mode=$ARGV[0];
+my $dir=$ARGV[1];
+
+find(\&check_files,($dir));
+
+switch ($mode){
+	case "time"	{&sort_by_modify_time(\%files_by_modify_time);}
+	case "size"	{&sort_by_size(\%files_by_size);}
+}
+
+%files_by_modify_time=();
+%files_by_size=();
+undef %files_by_modify_time;
+undef %files_by_size;
+undef $mode;
+undef $dir;
+
