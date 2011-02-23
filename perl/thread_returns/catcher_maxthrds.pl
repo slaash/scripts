92a79c786716111ec6c061aec82e87fe0a108c47
Radu Moisa <rmoisa@yahoo.com>
2011-02-23 16:48:47 +0200

    more thread stuff: max threads

---
+++ perl/thread_returns/catcher_maxthrds.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use threads;
+use threads::shared;
+
+my %hash :shared;
+
+sub display_array{
+	my $arr=$_[0];
+	my $cnt=$_[1];
+	my $ret;
+	for (@$arr){
+		$ret.="$_,";
+	}
+	chop $ret;
+	$hash{$cnt}=$ret;
+#	while (){}
+}
+
+my $arr1=["a","b","c","d"];
+my $arr2=["b","c","d","e"];
+my $arr3=["f","g","h","i"];
+my $arr4=["j","k","l","m"];
+my $arr5=["n","o","p","q"];
+
+my @arrs=($arr1,$arr2,$arr3,$arr4,$arr5);
+
+my @threads;
+my $cnt=0;
+while ($cnt<=$#arrs){
+	print "Starting thread $cnt\n";
+	my $arr=$arrs[$cnt];
+	my $thr=threads->create(\&display_array,$arr,$cnt);
+	if (scalar threads->list()<2){
+		push(@threads,$thr);
+	}
+	else{
+	       	$thr->join;
+	}
+	$cnt++;
+}
+
+foreach (@threads) {
+        $_->join;
+}
+
+print "All threads done\n";
+
+for (sort keys %hash){
+	print "$hash{$_}\n";
+}
+
