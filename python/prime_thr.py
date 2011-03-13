97d416ab747b9c2ed0277aeacffaa7fbae56fef1
slash <slash@vps3.home.net>
2011-03-13 22:51:13 +0300

    lots of thread stuff

--- python/prime_thr.py
+++ python/prime_thr.py
@@ -8,18 +8,29 @@ min=int(sys.argv[1])
 max=int(sys.argv[2])
 
 class thr (threading.Thread):
-        def run (self):
-		print("threads:"+str(threading.active_count()))
-                print("Started thread!\n")
+ def __init__ (self,n):
+  self.n = n
+  threading.Thread.__init__ ( self )
 
-def is_prime(n):
-	prim=1
-	for i in range(2,int(math.sqrt(n)+1)):
-		if math.fmod(n,i) == 0:
-			prim=0
-			break
-	if prim == 1:
-		print(n)
+ def run ( self ):
+  print("Got: "+self.n)
+  is_prime(self.n) 
+
+ def is_prime(n):
+  prim=1
+  for i in range(2,int(math.sqrt(n)+1)):
+   if math.fmod(n,i) == 0:
+    prim=0
+    break
+   if prim == 1:
+    print(n)
 
 for i in range(min,max+1):
-	is_prime(i)
+ t=thr(i)
+ runners=threading.active_count()
+ print("Active threads: "+str(runners))
+ if (runners<=4):
+  t.start
+ else:
+  t.join
+
