1e11756d2f62ff50b1d8d77efb6b291f4b431de2
rmoisa <rmoisa@hickory.osuosl.org>
2011-03-16 18:06:47 +0000

    fork in python

---
+++ python/prime_fork.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import math
+import sys
+import os
+import time
+
+min=int(sys.argv[1])
+max=int(sys.argv[2])
+
+def is_prime(n):
+	prim=1
+	for i in range(2,int(math.sqrt(n))+1):
+		if n % i == 0:
+			prim=0
+			break
+	if prim == 1:
+		print(n)
+	return 0
+
+parallel=5
+running=0;
+runners=[]
+
+for i in range(min,max+1):
+	pid=os.fork()
+	if pid==0:
+		is_prime(i)
+		exit()
+	else:
+#		print("Running children: "+str(running+1))
+		runners.append(pid)
+		running=running+1
+		if running>=parallel:
+			print("Waiting...\n")
+			os.wait()
+			running=running-1
+
+for pid in runners:
+	os.waitpid(pid,0)
+
