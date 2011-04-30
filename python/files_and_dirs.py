bc550b5a49526401cdbe6b2e087bc151b37bef8f
slash <rmoisa@yahoo.com>
2011-04-30 15:50:52 +0300

    some check

--- python/files_and_dirs.py
+++ python/files_and_dirs.py
@@ -13,23 +13,26 @@ else:
 	dir="/"
 
 def list_dir(dir):
-	files=os.listdir(dir)
-	time_fmt = "%d-%b-%Y %H:%M:%S"
-	for item in files:
-		item=os.path.join(dir,item)
-		if os.path.exists(item) == True:
-			size=os.path.getsize(item)
-			mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
-			if os.path.islink(item) == True:
-				dest=os.readlink(item)
-				print(item+"->"+dest+"  "+str(size)+" bytes  "+str(mod_time))
-			elif os.path.isfile(item) == True:
-				print(item+"  "+str(size)+" bytes  "+str(mod_time))
-			elif os.path.isdir(item) == True:
-				print(item+"/i "+str(size)+" bytes  "+str(mod_time))
-				list_dir(item+"/")
-		else:
-			print("Broken item: "+item)
+	try:
+		files=os.listdir(dir)
+		time_fmt = "%d-%b-%Y %H:%M:%S"
+		for item in files:
+			item=os.path.join(dir,item)
+			if os.path.exists(item) == True:
+				size=os.path.getsize(item)
+				mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
+				if os.path.islink(item) == True:
+					dest=os.readlink(item)
+					print(item+"->"+dest+"  "+str(size)+" bytes  "+str(mod_time))
+				elif os.path.isfile(item) == True:
+					print(item+"  "+str(size)+" bytes  "+str(mod_time))
+				elif os.path.isdir(item) == True:
+					print(item+"/i "+str(size)+" bytes  "+str(mod_time))
+					list_dir(item+"/")
+			else:
+				print(item+" is broken")
+	except:
+                print(dir+" can not be opened")
 
 list_dir(dir)
 
--- python/files_and_dirs_and_threads.py
+++ python/files_and_dirs_and_threads.py
@@ -22,30 +22,37 @@ class thr (threading.Thread):
 		self.dir = dir
 
 	def run ( self ):
+		print("Got "+self.dir)
 		global filez
-		files=os.listdir(self.dir)
-		time_fmt = "%d-%b-%Y %H:%M:%S"
-		for item in files:
-			item=os.path.join(self.dir,item)
-			if os.path.exists(item) == True:
-				size=os.path.getsize(item)
-				mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
-				if os.path.islink(item) == True:
-					dest=os.readlink(item)
-#					print(item+"->"+dest+"  "+str(size)+" bytes  "+str(mod_time))
-					filez[item]="->"+dest+"  "+str(size)+" bytes  "+str(mod_time)
-				elif os.path.isfile(item) == True:
-#					print(item+"  "+str(size)+" bytes  "+str(mod_time))
-					filez[item]="  "+str(size)+" bytes  "+str(mod_time)
-				elif os.path.isdir(item) == True:
-#					print(item+"/i "+str(size)+" bytes  "+str(mod_time))
-					filez[item]="/i "+str(size)+" bytes  "+str(mod_time)
-					t=thr(item+"/")
-					t.start()
-					if threading.active_count()>10:
-						t.join
-			else:
-				print("Broken item: "+item)
+		try:
+			files=os.listdir(self.dir)
+			time_fmt = "%d-%b-%Y %H:%M:%S"
+			for item in files:
+				item=os.path.join(self.dir,item)
+				if os.path.exists(item) == True:
+					size=os.path.getsize(item)
+					mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
+					if os.path.islink(item) == True:
+						dest=os.readlink(item)
+#						print(item+"->"+dest+"  "+str(size)+" bytes  "+str(mod_time))
+						filez[item]="->"+dest+"  "+str(size)+" bytes  "+str(mod_time)
+					elif os.path.isfile(item) == True:
+#						print(item+"  "+str(size)+" bytes  "+str(mod_time))
+						filez[item]="  "+str(size)+" bytes  "+str(mod_time)
+					elif os.path.isdir(item) == True:
+#						print(item+"/i "+str(size)+" bytes  "+str(mod_time))
+						filez[item]="/i "+str(size)+" bytes  "+str(mod_time)
+						t=thr(item+"/")
+						t.start()
+						if threading.active_count()>10:
+							t.join
+				else:
+#					print("Broken item: "+item)
+					filez[item]=" is broken"
+		except:
+#                        print("Can not open "+self.dir)
+			filez[self.dir]=" can not be opened"
+
 
 t=thr(dir)
 t.start()
