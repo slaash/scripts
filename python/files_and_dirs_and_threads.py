cfa19fb0f834354704c8baf5cae07b0b97246643
rmoisa <rmoisa@hickory.osuosl.org>
2011-05-01 13:21:38 +0000

    also search function :)

--- python/files_and_dirs_and_threads.py
+++ python/files_and_dirs_and_threads.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#usage: ./script <-r> <dir>
+#usage: ./script <-r> <-s pattern> <dir>
 
 import os
 import time
@@ -9,12 +9,15 @@ import threading
 import getopt
 
 sys.argv.pop(0)
-optlist,args=getopt.getopt(sys.argv,'r')
+optlist,args=getopt.getopt(sys.argv,'rs:')
 
 rec=False
 for o,a in optlist:
 	if o=="-r":
 		rec=True
+	if o=="-s":
+		search_expr=a
+
 if len(args)>0:
 	dir=args[0]
 	if re.match("(.+)/$",dir)==None:
@@ -35,24 +38,25 @@ class thr (threading.Thread):
 			files=os.listdir(self.dir)
 			time_fmt = "%d-%b-%Y %H:%M:%S"
 			for item in files:
-				item=os.path.join(self.dir,item)
-				if os.path.exists(item) == True:
-					size=os.path.getsize(item)
-					mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
-					if os.path.islink(item) == True:
-						dest=os.readlink(item)
-						filez[item]="->"+dest+"  "+str(size)+" bytes  "+str(mod_time)
-					elif os.path.isfile(item) == True:
-						filez[item]="  "+str(size)+" bytes  "+str(mod_time)
-					elif os.path.isdir(item) == True:
-						filez[item]="/ "+str(size)+" bytes  "+str(mod_time)
-						if rec==True:
-							t=thr(item+"/")
-							t.start()
-							if threading.activeCount()>10:
-								t.join
-				else:
-					filez[item]=" is broken"
+				if re.search(search_expr,item):
+					item=os.path.join(self.dir,item)
+					if os.path.exists(item) == True:
+						size=os.path.getsize(item)
+						mod_time=time.strftime(time_fmt,time.gmtime(os.path.getmtime(item)))
+						if os.path.islink(item) == True:
+							dest=os.readlink(item)
+							filez[item]="->"+dest+"  "+str(size)+" bytes  "+str(mod_time)
+						elif os.path.isfile(item) == True:
+							filez[item]="  "+str(size)+" bytes  "+str(mod_time)
+						elif os.path.isdir(item) == True:
+							filez[item]="/ "+str(size)+" bytes  "+str(mod_time)
+							if rec==True:
+								t=thr(item+"/")
+								t.start()
+								if threading.activeCount()>10:
+									t.join
+					else:
+						filez[item]=" is broken"
 		except:
 			filez[self.dir]=" can not be opened"
 
