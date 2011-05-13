16cb2d5001e19b840669ad50b89c99fbced26ded
Radu Moisa <uidl9555@iasp209x.ia.ro.conti.de>
2011-05-13 14:42:54 +0300

    the beginning

---
+++ php/etica/chestionar.php
@@ -0,0 +1,19 @@
+<html>
+<title>Chestionar</title>
+<body>
+<form name="chestionar" action="chestionar.php" method="POST">
+<p>Ce preferi sa mananci?</pr>
+<br/>
+<input type="radio" name="group1" value="Milk"> Milk<br>
+<input type="radio" name="group1" value="Butter" checked> Butter<br>
+<input type="radio" name="group1" value="Cheese"> Cheese
+<hr>
+<p>Ce preferi sa bei?</pr>
+<br/>
+<input type="radio" name="group2" value="Water"> Water<br>
+<input type="radio" name="group2" value="Beer"> Beer<br>
+<input type="radio" name="group2" value="Wine" checked> Wine<br>
+</form>
+</body>
+</html>
+
