b2326a0d8f07e8f7cb6264dc3e67927f86ea0995
Radu Moisa <rmoisa@yahoo.com>
2011-09-17 12:16:13 +0300

    perl 5.010 stuff

---
+++ perl/5010stuff/given.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use 5.010;
+
+#say "Shit!";
+
+#my @words=qw(New in Perl 5.010);
+#say for @words;
+
+#my $a;
+#my $b;
+#old stuff #$a=defined $b ? $b : 0;
+
+#new way
+#$a=$b || 0;
+#say "a: $a";
+
+#$a=$b // 0;
+#say "a: $a";
+
+#sub print_var{
+#	state $var=10;
+#	return $var--;
+#}
+#for (qw(1 2 3)){
+#	say &print_var;
+#}
+
+#if (-f -w "given.pl"){
+#	say "E acolo, e writable";
+#}
+
+#my $expr="I love regular expressions!!!";
+#my $match="I love regular expressions!";
+#my @arr=qw("I love regular expressions!" something else);
+#say for @arr;
+#if ($match ~~ $expr){
+#	say "1: Gotcha!";
+#}
+#if ($match ~~ @arr){
+#	say "2: Gotcha!";
+#}
+
+#while (<>){
+#	given ($_){
+#		when (/exit/){
+#			exit;
+#		}
+#		when (/hello/){
+#			say "Hello 2 U 2!";
+#		}
+#		default {
+#			say "Say smth!";
+#		}
+#	}
+#}
+
+#my @have=qw(I love regular expressions!!!);
+#my @want=@have->where("regular");
+#say @want;
+
