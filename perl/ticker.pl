47b73b88e976873880f164bd6c3f65a4bb59abf7
Radu Moisa <rmoisa@yahoo.com>
2011-10-06 10:17:47 +0300

    cleanup

--- perl/ticker.pl
+++ perl/ticker.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use 5.012;
+
 use threads;
 use threads::shared;
 
@@ -21,21 +23,19 @@ sub ticker{
 		sleep($wait);
 	}
 	print "\texiting ticker...\n";
-	threads->exit;
 }
 
 sub watcher{
 	my $wait=$_[0];
 	print "\t\t".threads->self()->tid()." - watcher: started timer for $wait sec\n";
 	while($string ne "exit" and $string ne "exit watcher"){
-		print "\t\t".localtime.": threads available\n";
+		print "\t\t".localtime().": threads available\n";
 		for (threads->list(threads::running)){
 			print "\t\t".$_->tid()."\n";
 		}
 		sleep($wait);
 	}
 	print "\t\texiting watcher...\n";
-	threads->exit;
 }
 
 my $ticker=threads->create(\&ticker,"1");
