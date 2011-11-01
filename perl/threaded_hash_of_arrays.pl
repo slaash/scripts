d28be8d9170db26fb22e267f3af60b05937dfe6e
Radu Moisa <rmoisa@yahoo.com>
2011-11-01 10:18:35 +0200

    -

--- perl/threaded_hash_of_arrays.pl
+++ perl/threaded_hash_of_arrays.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use 5.010;
+#use 5.010;
 
 use threads;
 use threads::shared;
@@ -15,31 +15,51 @@ my $crt : shared;
 $crt=0;
 
 sub add_an_array{
-	{
-		lock $crt;
-		$crt++;
-	}
+	lock $crt;
+	$crt++;
 	my @array;
-	push(@array,int(rand(100))) for (1..4);
+#	push(@array,int(rand(1000))) for (1..1000);
+	push(@array,0) for (1..1000);
+#	@array=sort {$a<=>$b} @array;
 	$hash_of_arrays{$crt}=shared_clone(\@array);
-#	return @array;
+	print "T";
 }
 
 my @threads;
-for (1..10){
-	my $thr=threads->create(\&add_an_array);
-	push (@threads,$thr);
+for (1..900){
+	my $thr;
+	$thr=threads->create(\&add_an_array);
+	if (!defined $thr){
+		die "could not create thread...";
+	}
+	if (threads->list(threads::running)<=10){
+		push (@threads,$thr);
+		print "A";
+	}
+	else{
+		print "J";
+		$thr->join();
+	}
 }
+print "\n";
 
+print $#threads." threads remaining\n";
 for (0..$#threads){
+	print "F";
 	$threads[$_]->join();
 }
 
-for (sort (keys %hash_of_arrays)){
-	print "$_: ";
-	for (@{$hash_of_arrays{$_}}){
-		print "$_ ";
-	}
-	print "\n";
-}
+my @keys=keys %hash_of_arrays;
+
+#for (sort {$a<=>$b} @keys){
+#	print "$_: ";
+#	for (@{$hash_of_arrays{$_}}){
+#		print "$_ ";
+#	}
+#	print "\n";
+#}
+
+print "\n";
+print $#keys+1;
+print " keys in hash\n";
 
