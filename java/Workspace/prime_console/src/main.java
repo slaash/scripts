926eee885558ac2a47bd2eba3dfe9f0ae60cf068
Radu Moisa <rmoisa@yahoo.com>
2011-12-02 15:18:21 +0200

    some shitty changes

--- java/Workspace/prime_console/src/main.java
+++ java/Workspace/prime_console/src/main.java
@@ -1,18 +1,12 @@
-import java.lang.*;
 import java.math.BigDecimal;
 
 class prime_console{
-	
-	private static int masterAlive=1;
-	
+
+	static int masterAlive=1;
+	static ThreadGroup MyThGroup=new ThreadGroup("Primes");
+
 	public static class Watcher extends Thread{
-		
-//		double n;
-//		
-//		Watcher(double n){
-//			this.n=n;
-//		}
-//		
+
 		public void run(){
 			while (masterAlive==1){
 				try {
@@ -20,56 +14,68 @@ class prime_console{
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				System.out.println("Threads: "+Integer.toString(Thread.activeCount()));
+				
+				System.out.println("Threads in " + Thread.currentThread().getThreadGroup().getName() + " : "+Integer.toString(Thread.currentThread().getThreadGroup().activeCount()));
+				System.out.println("Threads in "+ MyThGroup.getName() +": "+Integer.toString(MyThGroup.activeCount()));
+				System.out.println(Runtime.getRuntime().freeMemory()/1024/2014+ " MB free of " + Runtime.getRuntime().totalMemory()/1024/2014 + " MB Total, MAX is " + Runtime.getRuntime().maxMemory()/1024/2014 + " MB");
 			}
+			System.out.println("Watcher done!");
 		}	
-	
+
 	}
 	public static class MyThread extends Thread{
-		
+
 		double n;
-		
-		MyThread(double n){
-			this.n=n;
+
+		MyThread(ThreadGroup tg, String n){
+			super(tg,n);
+			this.n=Double.valueOf(n);
 		}
-		
+
 		public void run(){
+//			System.out.println(Thread.currentThread().getThreadGroup().getName());
 			is_prime(n);
 		}
-	
+
 		public void is_prime(double n){
 			boolean prim=true;
 			double j;
-//			try {
-//				Thread.sleep(5000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-	        for (j=2;j<=Math.sqrt(n);j++){
+	        for (j=2;j<=Math.sqrt(n);j=j+1){
 	                if (n % j==0){
 	                        prim=false;
 	                        break;
 	                }
 	        }
-	        if (prim){
+	        if (prim==true){
 	                System.out.println(BigDecimal.valueOf(n));
 	        }
 		}
 
 	}
-	
-	public static void main (String[] args){
-		
+
+	public static void main (String[] args) throws InterruptedException{
+
 		Watcher w=new Watcher();
 		w.start();
-		
-		double min=new BigDecimal("10000000000000000").doubleValue();
-		double max=new BigDecimal("10000000000000100").doubleValue();
+
+		double min;
+		double max;
+
+		if (args.length>0){
+
+			min=Double.valueOf(args[0]);
+			max=Double.valueOf(args[1]);
+		}
+		else{
+			min=Double.valueOf("1");
+            max=Double.valueOf("100");
+        }
+
 		double i;
-		int cnt=0;
 		for (i=min;i<=max;i++){
-			MyThread t=new MyThread(i);
-			if (Thread.activeCount()<10){
+			MyThread t=new MyThread(MyThGroup,String.valueOf(i));
+//			if (Thread.currentThread().getThreadGroup().activeCount()<10){
+			if (MyThGroup.activeCount()<10){
 				t.start();
 			}
 			else{
@@ -79,12 +85,24 @@ class prime_console{
 					e.printStackTrace();
 				}
 			}
-			cnt++;
 		}
-		while (Thread.activeCount()>2){
-			
-		}
+//		while (Thread.currentThread().getThreadGroup().activeCount()>2){
+//		while (MyThGroup.activeCount()>0){
+//		}
+		
+//		int count = MyThGroup.activeCount() * 2; // factor of 2 for safety 
+		int count=(int) (max-min+1);
+		Thread[] threads = new Thread[(int) (max-min+1)];  
+		MyThGroup.enumerate(threads);  
+	    for (int j = 0; j < count; j++)  
+	    {  
+	      Thread t = threads[j];  
+	      if (t != null)  
+	        t.join();  
+	    }  
+				
 		masterAlive=0;
+		w.join();
 		System.out.println("Master done!");
 	}
 
