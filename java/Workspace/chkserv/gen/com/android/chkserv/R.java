520522fc804d825a726aea354faeec3f573a5129
unknown <Stefana@laptop.(none)>
2011-12-25 11:25:34 +0200

    spawn new app!

--- java/Workspace/chkserv/gen/com/android/chkserv/R.java
+++ java/Workspace/chkserv/gen/com/android/chkserv/R.java
@@ -14,17 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050002;
         public static final int checkBox1=0x7f050000;
         public static final int checkBox2=0x7f050001;
+        public static final int editText1=0x7f050004;
+        public static final int relativeLayout1=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main2=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int hintEdit=0x7f040007;
         public static final int labelCheckbox=0x7f040002;
         public static final int labelReset=0x7f040003;
+        public static final int labelSave=0x7f040005;
+        public static final int labelSpawn=0x7f040006;
         public static final int labelStore=0x7f040004;
     }
 }
--- java/Workspace/chkserv/src/com/android/chkserv/ChkservActivity.java
+++ java/Workspace/chkserv/src/com/android/chkserv/ChkservActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
@@ -77,6 +78,12 @@ public class ChkservActivity extends Activity {
             }
         });
         
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(new Intent(ChkservActivity.this,newactivity.class));
+            }
+        });
     }
     
     protected void onStop() {
