3390c9bb2d9dd3b9710b8dea5ba3694c56b3b566
Radu Moisa <rmoisa@yahoo.com>
2012-01-29 19:53:35 +0000

    selektor+garbage

--- perl/oop/selektor.pl
+++ perl/oop/selektor.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use Getopt::Long;
 
 use selektor;
 
@@ -11,17 +12,36 @@ my @list=("Gigi","Vali","Simi","Mimi","Fifi");
 
 sub displayList{
 	for (@_){
-		print " $_\n";
+		print "$_ ";
 	}
+	print "\n";
 }
 
-my $x=new selektor(@list);
-print "List:\n";
-&displayList($x->getNames());
+my $debug=0;
+GetOptions("verbose" => \$debug);#sets it to 1 if present
 
-for (0..$#list){
-	print "\tDeleted: ".$x->remRandName()."\n";
-	print "New list:\n";
-	displayList($x->getNames());
+system("clear");
+
+my $x=new selektor($debug,@list);
+
+for (@list){
+	print "Full list: ";
+	&displayList(@list);
+	print "\t$_ extracts... ";
+	if ($debug == 0){
+		<>;
+		print "\t";
+	}
+	print $x->remRandName($_)."\n";
+	if ($debug == 1){
+		print "Remains:   ";
+		&displayList($x->getNames());
+	}
+	if ($debug == 0){
+		print "Press a key to continue...\n";
+		<>;
+		system("clear");
+	}
+	
 }
 
--- perl/oop/selektor.pm
+++ perl/oop/selektor.pm
@@ -1,8 +1,10 @@
 package selektor;
 
 sub new{
-#usage: new(("gigi","vali","simi"));
+#usage: new(<0|1>,("gigi","vali","simi"));
 	my $self=shift;
+	$self->{verbose}=$_[0];
+	shift;
 	@{$self->{list}}=@_;
 	bless({$self});
 	return $self;
@@ -15,11 +17,36 @@ sub getNames{
 }
 
 sub remRandName{
-#delete an element from the names array from a random position
+#usage: remRandName("gigi")
+#gets the element from a random position in the list
+#delete the element from the list on the random position
+#IF IT IS NOT IDENTICAL WITH THE SUPPLIED PARAMETER
 	my ($self)=shift;
 	my $namesInList=$#{$self->{list}}+1;
-	my $randPos=int(rand($namesInList));
+	my $randPos;
+	#if there are only two remaining in the list, compare with the supplied parameter and return the other element
+	if ($namesInList == 2){
+		if ($self->{verbose} == "1"){
+			print "2 remaining... ";
+		}
+		if (${$self->{list}}[0] eq $_[0]){
+			$randPos=1;
+		}
+		else{
+			$randPos=0;
+		}
+	}
+	else{
+		$randPos=int(rand($namesInList));
+	}
 	my $delName=${$self->{list}}[$randPos];
+	while ($delName eq $_[0]){
+		if ($self->{verbose} == "1"){
+			print "$delName eq $_[0], extracting again... ";
+		}
+		$randPos=int(rand($namesInList));
+		$delName=${$self->{list}}[$randPos];
+	}
 	splice(@{$self->{list}},$randPos,1);#better than delete!
 	return $delName;
 }
