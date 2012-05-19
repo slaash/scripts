01d8abd9e5a9573f520a784244a202278735c9c4
Radu Moisa <rmoisa@yahoo.com>
2012-05-19 16:43:11 +0000

    platf info

--- python/system.py
+++ python/system.py
@@ -2,21 +2,56 @@
 
 import os
 import time
+import platform as pl
 
-unumber = os.getuid()
-pnumber = os.getpid()
-where = os.getcwd()
-what = os.uname()
-used = os.times()
-now = time.time()
-means = time.ctime(now)
+def showSomeInfo(d):
+	print("---")
+	for i in d.keys():
+		s=""
+		if (type(d[i])==type((1,2))):
+			s+=i+": "
+			for j in d[i]:
+				s+=str(j)+" "
+			print(s)
+		else:
+	        	print("{0}: {1}".format(i,d[i]))
+	print("---")
 
-print("User number",unumber)
-print("Process ID",pnumber)
-print("Current Directory",where)
-print("System information",what)
-print("System information",used)
 
-print("\nTime is now",now)
-print("Which interprets as",means)
+def osInfo():
+	try:
+		info={'UID' : os.getuid(),
+			'PID' : os.getpid(),
+			'PWD' : os.getcwd(),
+			'UNAME' : os.uname(),
+			'Sys info' : os.times()}
+	except AttributeError as err1:
+		print("Error in getting os info: "+str(err1))
+		info={}
+	return(info)
+
+def timeInfo():
+	now = time.time()
+	means = time.ctime(now)
+
+def platfInfo():
+	try:
+		info={'arch' : pl.architecture(),
+			'mach' : pl.machine(),
+			'node' : pl.node(),
+			'platform' : pl.platform(),
+			'cpu' : pl.processor(),
+			'python impl' : pl.python_implementation(),
+			'python ver' : pl.python_version(),
+			'release' : pl.release(),
+			'system' : pl.system(),
+			'version' : pl.version(),
+			'dist' : pl.linux_distribution()}
+	except AttributeError as err1:
+		print("Error in getting platform info: "+str(err1))
+		info={}
+	return(info)
+
+showSomeInfo(osInfo())
+showSomeInfo(platfInfo())
 
