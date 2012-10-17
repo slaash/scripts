cb8d9923e1d113b5faab602e99a33c0e91c5dcd1
Radu Moisa <rmoisa@yahoo.com>
2012-10-17 11:21:28 +0300

    linkedin again

---
+++ python/linkedin.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import oauth2 as oauth
+import httplib2
+import time, os, simplejson as json
+
+urls={'user_prof' : 'http://api.linkedin.com/v1/people/~',
+	'conns' : 'http://api.linkedin.com/v1/people/~/connections'
+}
+ 
+# Fill the keys and secrets you retrieved after registering your app
+consumer_key      =   'zeah1srfpllc'
+consumer_secret  =   'kNhQGi1Clm1yRF8t'
+user_token           =   '4d2b5bbe-107a-424e-9e97-23ea78d7b5c5'
+user_secret          =   '84bfdc05-5c69-4039-b3c4-64a7845e8e0e'
+ 
+# Use your API key and secret to instantiate consumer object
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+ 
+# Use your developer token and secret to instantiate access token object
+access_token = oauth.Token(
+            key=user_token,
+            secret=user_secret)
+ 
+client = oauth.Client(consumer, access_token)
+ 
+# Make call to LinkedIn to retrieve your own profile
+#resp,content = client.request("http://api.linkedin.com/v1/people/~", "GET", {})
+ 
+# By default, the LinkedIn API responses are in XML format. If you prefer JSON, simply specify the format in your call
+for comm in urls.keys():
+	resp,content = client.request(urls[comm]+"?format=json&count=1", "GET")
+	d=json.loads(content)
+	for item in d.keys():
+		print("{} => {}".format(item,d[item]))
+	
