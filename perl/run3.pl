2cc41721dc5efc0fc5e46de2abfa7f90dde1d34d
Radu Moisa <rmoisa@yahoo.com>
2012-11-03 11:19:19 +0200

    run3

--- perl/run3.pl
+++ perl/run3.pl
@@ -5,6 +5,10 @@ use warnings;
 use IPC::Run3;
 
 my $out;
-run3(['echo', 'dsa"dsa`'], \undef, \$out);
-print $out;
+my $err;
+
+run3(join(' ',@ARGV), \undef, \$out,\$err);
+print "STDOUT: $out\n";
+print "STDERR: $err\n";
+print "EXI CODE: $?\n";
 
