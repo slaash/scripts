0541b6c8a586cb1fa9f7a5340753279e3b7de881
Radu Moisa <rmoisa@yahoo.com>
2012-11-24 21:43:46 +0200

    perl find

--- python/threaded_csv/find.py
+++ python/threaded_csv/find.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 
-import os,sys
+import os, sys
 
-def findItem(d):
+def find_item(d):
 	try:
-		files=os.listdir(d)
+		files = os.listdir(d)
 	except OSError, err:
 		print(d+": "+str(err))
 	else:
 		print(d)
 		for item in files:
-			fullitem=os.path.join(d,item)
+			fullitem = os.path.join(d, item)
 			if (os.path.isdir(fullitem)):
-				findItem(fullitem)
+				find_item(fullitem)
 			else:
 				print(fullitem)
 
 if (len(sys.argv)>1):
-	d=sys.argv[1]
+	s_dir = sys.argv[1]
 else:
-	d="./"
-findItem(d)
+	s_dir = "./"
+find_item(s_dir)
 
--- python/threaded_csv/find_thr.py
+++ python/threaded_csv/find_thr.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 
 from multiprocessing.pool import Pool
-import os,sys,re
+import os, sys
 
-def findItem(d):
+def find_item(d):
 	try:
-		files=os.listdir(d)
+		files = os.listdir(d)
 	except OSError, err:
 		print(d+": "+str(err))
 	else:
 		print(d)
 		for item in files:
-			fullitem=os.path.join(d,item)
+			fullitem = os.path.join(d, item)
 			if (os.path.isdir(fullitem)):
-				tPool.apply_async(findItem,[fullitem])
+				tPool.apply_async(find_item, [fullitem])
 #				findItem(fullitem)
 			else:
 				print(fullitem)
 
 if (len(sys.argv)>1):
-	d=sys.argv[1]
+	s_dir = sys.argv[1]
 else:
-	d="./"
+	s_dir = "./"
 
-tPool=Pool(2)
-tPool.apply(findItem,[d,])
+tPool = Pool(2)
+tPool.apply(find_item, [s_dir])
 tPool.close()
 tPool.join()
 #findItem(d)
