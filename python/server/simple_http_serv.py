5da2e1afb695665243f0b2389ba1a18315d68fb6
Radu Moisa <rmoisa@yahoo.com>
2012-11-25 18:24:42 +0200

    -

--- python/server/simple_http_serv.py
+++ python/server/simple_http_serv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-import BaseHTTPServer 
-import SocketServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
 import csv, time, sys, os.path
 
-class CustomHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class CustomHandler(BaseHTTPRequestHandler):
 
 	server_version = "server"
 	sys_version = "system"
@@ -24,7 +24,7 @@ class CustomHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	def toCSV(self):
 		outputCSV.writerow((time.strftime('%d-%m-%Y %H:%M:%S'), self.address_string(), self.client_address[0], self.client_address[1], self.command, self.path, self.request_version))
 
-class thrSimpleHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+class thrSimpleHTTPServer(ThreadingMixIn, HTTPServer):
 	pass
 
 PORT = 8000
@@ -34,7 +34,7 @@ outputCSV = csv.writer(outputFile, delimiter = ',', quotechar = '"', quoting = c
 
 httpd = thrSimpleHTTPServer(("", PORT), CustomHandler)
 
-print("serving at port"+str(PORT))
+print("serving at port "+str(PORT))
 print("using file "+os.path.join(sys.path[0],'output.csv'))
 try:
 	httpd.serve_forever()
