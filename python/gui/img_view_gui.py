a9ae68a756b9fe0714a5312bb7f777ab66d9919e
Radu Moisa <rmoisa@yahoo.com>
2012-12-03 19:35:32 +0200

    -

--- python/gui/img_view_gui.py
+++ python/gui/img_view_gui.py
@@ -2,43 +2,51 @@
 
 # Form implementation generated from reading ui file 'img_view_gui.ui'
 #
-# Created: Sat Dec  1 19:35:56 2012
-#      by: PyQt4 UI code generator 4.9.3
+# Created: Mon Dec  3 12:33:09 2012
+#      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    _fromUtf8 = lambda s: s
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(800, 600)
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(943, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 540, 75, 23))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton.setGeometry(QtCore.QRect(10, 530, 91, 23))
+        self.pushButton.setObjectName("pushButton")
         self.frame = QtGui.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(10, 10, 781, 521))
+        self.frame.setGeometry(QtCore.QRect(0, 0, 941, 521))
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
+        self.frame.setObjectName("frame")
         self.label = QtGui.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(10, 10, 761, 501))
-        self.label.setMinimumSize(QtCore.QSize(501, 0))
-        self.label.setObjectName(_fromUtf8("label"))
+        self.label.setGeometry(QtCore.QRect(10, 10, 451, 481))
+        self.label.setMinimumSize(QtCore.QSize(0, 0))
+        self.label.setObjectName("label")
+        self.label_2 = QtGui.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(480, 0, 451, 491))
+        self.label_2.setMinimumSize(QtCore.QSize(0, 0))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtGui.QLabel(self.frame)
+        self.label_3.setGeometry(QtCore.QRect(10, 500, 451, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtGui.QLabel(self.frame)
+        self.label_4.setGeometry(QtCore.QRect(480, 500, 451, 16))
+        self.label_4.setObjectName("label_4")
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(480, 530, 91, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 19))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 943, 19))
+        self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -46,6 +54,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Show", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("MainWindow", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Browse", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "QPixmap", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("MainWindow", "OpenCV", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Original image", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("MainWindow", "Filter used", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Change filter", None, QtGui.QApplication.UnicodeUTF8))
 
--- python/gui/img_view_main.py
+++ python/gui/img_view_main.py
@@ -2,7 +2,8 @@
 
 from PyQt4 import QtCore, QtGui
 from img_view_gui import Ui_MainWindow
-import sys, time
+import sys, time, os.path
+import cv
 
 class MyForm(QtGui.QMainWindow):
 
@@ -11,15 +12,40 @@ class MyForm(QtGui.QMainWindow):
 		self.ui=Ui_MainWindow()
 		self.ui.setupUi(self)
 		self.ui.pushButton.clicked.connect(self.displayImg)
+		self.ui.pushButton_2.clicked.connect(self.showFace)
 
 	def displayImg(self):
-		imgFile=self.getOpenDialogRes()
-		if (imgFile != ''):	
-			self.ui.label.setPixmap(QtGui.QPixmap(imgFile).scaled(self.ui.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
+		self.imgFile=self.getOpenDialogRes()
+		if (self.imgFile != ''):	
+			self.ui.label.setPixmap(QtGui.QPixmap(self.imgFile).scaled(self.ui.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
 
 	def getOpenDialogRes(self):
 		filename=QtGui.QFileDialog.getOpenFileName(None, "Select image", "", "*.*", None)
 		return filename
+
+	#begin OpenCV part
+
+	def showFace(self):
+		print(self.imgFile)
+		im=cv.LoadImageM(self.imgFile)
+		print("W/H: {}/{}".format(*cv.GetSize(im)))
+		#resize to 800x???
+		if (args.nores==False):
+			im=resizeImg(im)
+		#
+		faces=cv.HaarDetectObjects(im,hc,cv.CreateMemStorage(),1.2,2,cv.CV_HAAR_DO_CANNY_PRUNING)
+		for (x,y,w,h),n in faces:
+			cv.Rectangle(im,(x,y),(x+w,y+h),255)
+		cv.ShowImage('display',im)
+		c=cv.WaitKey(0)
+		print('Pressed '+str(c))
+		if (c==1048603):
+			exit(0)
+
+	#end OpenCV part
+
+haarPath='/usr/share/opencv/haarcascades'
+hc=cv.Load(os.path.join(haarPath,'haarcascade_frontalface_default.xml'))
 
 app=QtGui.QApplication(sys.argv)
 myapp=MyForm()
