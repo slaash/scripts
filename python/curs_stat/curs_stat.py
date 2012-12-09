8c1e56f5836261d8b25d1d5e8decba6d65f2b230
Radu Moisa <rmoisa@yahoo.com>
2012-12-09 20:27:11 +0200

    working xml...

---
+++ python/curs_stat/curs_stat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+
+file='/home/slash/nbrfxrates2012.xml'
+#file='./test.xml'
+
+tree=ET.parse(file)
+root=tree.getroot()
+for elem in root.findall('Body/Cube'):
+	print(elem.get('date'))
+	for elem2 in elem.findall("Rate[@currency='EUR']"):
+#		print(elem2.get('currency'))
+		print(elem2.text)
+
