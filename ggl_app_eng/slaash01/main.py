1544b2aa8ac801b4c09dbd1c4538f58404029cc5
Radu Moisa <rmoisa@yahoo.com>
2013-01-02 16:38:44 +0200

    datastore

--- ggl_app_eng/slaash01/main.py
+++ ggl_app_eng/slaash01/main.py
@@ -14,14 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
-from google.appengine.api import users
-import os,time
+import os
+import time
 import platform as pl
 import cgi
 import json
 
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import db
+
+class VisitorInfo(db.Model):
+	date = db.DateTimeProperty(auto_now_add=True)
+	ip = db.StringProperty()
+	nick = db.StringProperty()
+	email = db.StringProperty()
+
 class BaseHandler(webapp2.RequestHandler):
+
+	def __init__(self, request, response):
+		self.initialize(request, response)
+		self.user = users.get_current_user()
+
+	def dispatch(self):
+		if (self.user):
+			super(BaseHandler, self).dispatch()
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
 	def _print(self,txt):
 		self.response.out.write(txt)
 		self._br()
@@ -33,27 +54,32 @@ class BaseHandler(webapp2.RequestHandler):
 		self.response.out.write('<br/>'+"\n")
 
 class MainHandler(BaseHandler):
+
+	def setVisInfo(self,ip,nick,email):
+		vis=VisitorInfo()
+		vis.ip=ip
+		vis.nick=nick
+		vis.email=email
+		vis.put()
+
 	def get(self):
-		user = users.get_current_user()
-		if (user):
-			now=time.ctime(time.time())
-			self._print('{} - Hello {} ({}) !'.format(now,user.nickname(),user.email()))
-			self._hr()
-			self._print("Your IP: "+self.request.remote_addr)
-			self._print("Origin URL: "+self.request.url)
-			self._print("uid: "+str(os.getuid()))
-			self._print("pid: "+str(os.getpid()))
-			self._print("cwd: "+os.getcwd())
-			self._print(pl.system()+", "+pl.architecture()[0])
-			self._print(pl.python_implementation()+", "+pl.python_version())
-			self._hr()
-			self._form("/primez","de la","la")
-			self._hr()
-			self.response.out.write("<a href='/logoff'>LogOut_1</a>")
-			self._br()	
-			self.response.out.write("<a href='"+users.create_logout_url("/")+"'>LogOut_2</a>")
-		else:
-			self.redirect(users.create_login_url(self.request.uri))
+		self.setVisInfo(self.request.remote_addr,self.user.nickname(),self.user.email())
+		now=time.ctime(time.time())
+		self._print('{} - Hello {} ({}) !'.format(now,self.user.nickname(),self.user.email()))
+		self._hr()
+		self._print("Your IP: "+self.request.remote_addr)
+		self._print("Origin URL: "+self.request.url)
+		self._print("uid: "+str(os.getuid()))
+		self._print("pid: "+str(os.getpid()))
+		self._print("cwd: "+os.getcwd())
+		self._print(pl.system()+", "+pl.architecture()[0])
+		self._print(pl.python_implementation()+", "+pl.python_version())
+		self._hr()
+		self._form("/primez","de_la","la")
+		self._hr()
+		self.response.out.write("<a href='/logoff'>LogOut_1</a>")
+		self._br()	
+		self.response.out.write("<a href='"+users.create_logout_url("/")+"'>LogOut_2</a>")
 
 	def _form(self,*args):
 		url=args[0]
@@ -75,16 +101,27 @@ class PrimezHandler(BaseHandler):
 	def post(self):
 		qtype=cgi.escape(self.request.get('qtype'))
 		if (qtype=='HTML'):
-			self._print(cgi.escape(self.request.get('de la')))
+			self._print(cgi.escape(self.request.get('de_la')))
 			self._print(cgi.escape(self.request.get('la')))
 		elif (qtype=='JSON'):
 			d={}
-			d['de la']=cgi.escape(self.request.get('de la'))
+			d['de_la']=cgi.escape(self.request.get('de_la'))
 			d['la']=cgi.escape(self.request.get('la'))
 			self.response.out.write(json.dumps(d))
+
+	def get(self):
+		self.post()
+
+class VisitorsHandler(BaseHandler):
+	def get(self):
+		rez=db.GqlQuery("select * from VisitorInfo")
+		for line in rez:
+			self._print("{} | {} | {} | {}".format(line.date,line.ip,line.nick,line.email))
+			self._hr()	
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler),
 	('/logoff', LogoffHandler),
-	('/primez', PrimezHandler)
+	('/primez', PrimezHandler),
+	('/who', VisitorsHandler)
 ], debug=True)
