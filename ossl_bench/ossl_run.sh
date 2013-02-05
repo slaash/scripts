22c2308b740d3cc98595d727533c6099d66669f4
Radu Moisa <rmoisa@yahoo.com>
2013-02-05 18:30:35 +0000

    -

--- ossl_bench/ossl_run.sh
+++ ossl_bench/ossl_run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-c="md5"
+if [ $1 ]; then
+	c=$1
+else
+	c="md5"
+fi
 
-cmd="openssl speed"
+cmd=`which openssl`
+
+uname -a
+grep "model name" /proc/cpuinfo|uniq -c
 
 for m in 1 2 10 100
 do
-	echo $m
-	echo "encrypt"	
-	$cmd -multi $m $c 2>/dev/null|grep -E "^$c"
-	echo "decrypt"
-	$cmd -decrypt -multi $m $c 2>/dev/null|grep -E "^$c"
+	echo "encrypt/decrypt - $m procs"
+	$cmd speed -multi $m $c 2>/dev/null|grep -E "^$c"
+	echo "-"
+	$cmd speed -decrypt -multi $m $c 2>/dev/null|grep -E "^$c"
 done
 
