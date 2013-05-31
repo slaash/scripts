ce2eb9cf967ca31976ad28a1643ebf90f80b6d98
Radu Moisa <rmoisa@yahoo.com>
2013-05-31 12:46:18 +0300

    -

--- kismyfile.sh
+++ kismyfile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-csvtool -t ";" -u TAB col 1,2,3,4,6,8,10,18,21,23,37,38 $1
+if [ -z $1 ]; then
+	find /var/log/kismet -name "*.csv"
+else
+	csvtool -t ";" -u TAB col 1,2,3,4,6,8,10,17,18,21,23,37,38 $1
+fi
 
