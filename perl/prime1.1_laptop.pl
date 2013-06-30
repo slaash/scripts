c4c3ac10ac8e3b89250033bcf9b5f62cdba5cb87
Radu Moisa <rmoisa@yahoo.com>
2013-06-30 09:17:09 +0300

    perlcritic

--- perl/prime1.1_laptop.pl
+++ perl/prime1.1_laptop.pl
@@ -1,21 +1,26 @@
 #!/usr/bin/perl
 
+#$Id$
+#$Revision$
+#$Date$
+
 use strict;
 use warnings;
 
-my $min=$ARGV[0];
-my $max=$ARGV[1];
+our $VERSION = 1;
 
-for my $i ($min..$max){
-	my $prim=1;
-	for my $j (2..sqrt($i)){
-		if ($i%$j==0){
-			$prim=0;
+my $min = $ARGV[0];
+my $max = $ARGV[1];
+
+for my $i ( $min .. $max ) {
+	my $prim = 1;
+	for my $j ( 2 .. sqrt $i ) {
+		if ( $i % $j == 0 ) {
+			$prim = 0;
 			last;
 		}
 	}
-	if ($prim==1){
-		print "$i\n";
+	if ( $prim == 1 ) {
+		print "$i\n" or croak 'aaa';
 	}
 }
-
