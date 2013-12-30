8efb66153eaa263298c7c469b2bfb8d2a34fb32c
Radu Moisa <rmoisa@yahoo.com>
2013-12-30 18:10:26 +0200

    omxplayer

---
+++ omx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ctrls="Key bindings
+Key Action
+1 Increase Speed
+2 Decrease Speed
+j Previous Audio stream
+k Next Audio stream
+i Previous Chapter
+o Next Chapter
+n Previous Subtitle stream
+m Next Subtitle stream
+s Toggle subtitles
+q Exit OMXPlayer
+Space or p Pause/Resume
+- Decrease Volume
++ Increase Volume
+Left Seek -30
+Right Seek +30
+Down Seek -600
+Up Seek +600"
+
+omxplayer -o hdmi --align center "${1}"
+
