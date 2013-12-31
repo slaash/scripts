a94c9fd109ef87d5ec4a96bf36cdd9edcef3b900
Radu Moisa <rmoisa@yahoo.com>
2013-12-31 18:22:55 +0200

    /2

--- largest_pal/lp.pl
+++ largest_pal/lp.pl
@@ -1,26 +1,32 @@
 #!/usr/bin/perl
 
-my $orig=$ARGV[0];
-chomp $orig;
+#my $orig=$ARGV[0];
+#chomp $orig;
+#print "trying $orig...\n";
 
 sub is_pal{
 	my $orig=$_[0];
 	my $is_pal=1;
-	for (my $i=0;$i<length($orig);$i++){
+	for (my $i=0;$i<length($orig)/2;$i++){
 		if (substr($orig,$i,1) ne substr($orig,length($orig)-$i-1,1)){
 			$is_pal=0;
 			break;
 		}
 	}
 	if ($is_pal){
-		print "$orig\n";
-		exit;
+		print "---$orig---\n";
 	}
 }
 
-for ($i=length($orig);$i>=2;$i--){
-	for ($j=0;$j<=length($orig)-$i;$j++){
-		is_pal(substr($orig,$j,$i));
+while (<>){
+	my $orig=$_;
+	chomp $orig;
+#	print "trying $orig...\n";
+
+	for ($i=length($orig);$i>=2;$i--){
+		for ($j=0;$j<=length($orig)-$i;$j++){
+			is_pal(substr($orig,$j,$i));
+		}
 	}
 }
 
