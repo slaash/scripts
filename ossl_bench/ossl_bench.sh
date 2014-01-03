938b5c279a8b29af92efa461c868879aefe296c9
Radu Moisa <rmoisa@yahoo.com>
2014-01-03 16:33:09 +0200

    +arrays

--- ossl_bench/ossl_bench.sh
+++ ossl_bench/ossl_bench.sh
@@ -8,8 +8,8 @@ rsaf=$(mktemp -t "tmpXXX")
 host=$(uname -n)
 os=$(uname -s)
 arch=$(uname -m)
-ciphers="blowfish rsa4096"
-runs="1 2"
+ciphers=(blowfish rsa4096)
+runs=(1 2 10 100)
 
 if [[ -f /proc/meminfo && -f /proc/cpuinfo ]]; then
         mem=$(sed -En "s/MemTotal:\s+(.+)/\1/p" /proc/meminfo)
@@ -19,16 +19,20 @@ ${ossl} version
 echo "${host} ${os} ${arch} ${cpu} ${mem}"
 
 for n in ${runs[@]}; do
-	for c in ${ciphers};do
+	for c in ${ciphers[@]};do
 		${ossl} speed -multi ${n} ${c} >> ${tmpf} 2>/dev/null
 	done
 done
 
+cntb=0
+cntr=0
 while read line; do
 	if [[ $line =~ ^blowfish ]]; then
-		echo "${host}	${line}">>${blowf}
+		echo "${host}	${runs[${cntb}]}	${line}">>${blowf}
+		(( cntb+=1 ))
 	elif [[ $line =~ ^rsa\ 4096 ]];then
-		echo "${host}	${line}">>${rsaf}
+		echo "${host}	${runs[${cntr}]}	${line}">>${rsaf}
+		(( cntr+=1 ))
 	fi
 done < ${tmpf}
 
