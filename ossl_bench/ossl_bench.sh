ec4c67d8cceef3e13eb4ee99160d2006740ac89f
Radu Moisa <rmoisa@yahoo.com>
2014-01-03 15:49:36 +0200

    -

--- ossl_bench/ossl_bench.sh
+++ ossl_bench/ossl_bench.sh
@@ -15,6 +15,7 @@ if [[ -f /proc/meminfo && -f /proc/cpuinfo ]]; then
         mem=$(sed -En "s/MemTotal:\s+(.+)/\1/p" /proc/meminfo)
         cpu=$(sed -En "s/model name\s+:\s+(.+)/\1/p" /proc/cpuinfo)
 fi
+${ossl} version
 echo "${host} ${os} ${arch} ${cpu} ${mem}"
 
 for n in ${runs[@]}; do
