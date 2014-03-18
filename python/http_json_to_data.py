4a837d9acff5b08a1509b3d767b92af709106a06
Radu Moisa <rmoisa@yahoo.com>
2014-03-18 10:27:01 +0200

    added proxy, getopt, logfile

--- python/http_json_to_data.py
+++ python/http_json_to_data.py
@@ -3,6 +3,9 @@
 import urllib2
 import contextlib
 import simplejson as json
+import time
+import sys
+import getopt
 
 class infoStruct(object):
 #	def __init__(self, d):
@@ -12,26 +15,56 @@ class infoStruct(object):
 #			else:
 #				setattr(self, k, v)
 
-	def __init__(self, data=None):
+	def __init__(self, data = None):
 		object.__setattr__(self, '.data', {} if data is None else data)
 
 	def __call__(self):
 		return getattr(self, '.data')
 
 	def __getattr__(self, name):
-		if isinstance(self()[name],dict):
-			return infoStruct(self()[name])
+		if name in self().keys():
+			if isinstance(self()[name], dict):
+				return infoStruct(self()[name])
+			else:
+				return self()[name]
 		else:
-			return self()[name]
+			return ''
 
 #url="http://ipinfo.io/"
-url="https://www.dailycred.com/api/info.json"
-
+logFile = "/home/pi/location_info.txt"
+url = "https://www.dailycred.com/api/info.json"
+useProxy = 1
+logOnly = 0
+sys.argv.pop(0)
+try:
+	optlist, args = getopt.getopt(sys.argv, 'nu:l:o')
+	for o, a in optlist:
+		if o == '-n':
+			useProxy = 0
+		elif o == '-u':
+			url = a
+		elif o == '-l':
+			logFile = a
+		elif o == '-o':
+			logOnly = 1
+except getopt.GetoptError as err:
+	print(err)
+	sys.exit()
+if (useProxy == 1):
+	proxy = urllib2.ProxyHandler({'http': '127.0.0.1:8118', 'https': '127.0.0.1:8118'})
+	opener = urllib2.build_opener(proxy)
+	urllib2.install_opener(opener)
 with contextlib.closing(urllib2.urlopen(url)) as f:
-	data = json.load(f)
+	locData = json.load(f)
+iS = infoStruct(locData)
+if (logOnly == 0):
+	print(iS.local.city, iS.local.countryName)
+	print(iS.local.longitude, iS.local.latitude)
+	print(iS.ip.ip)
+try:
+	f = open(logFile, 'a')
+	f.write("{},{},{},{},{},{}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"), iS.local.city, iS.local.countryName, iS.local.longitude, iS.local.latitude, iS.ip.ip))
+	f.close()
+except IOError as err:
+	print(err)
 
-iS=infoStruct(data)
-
-print iS.local.city, iS.local.countryName
-print iS.local.longitude, iS.local.latitude
-print iS.ip.ip
