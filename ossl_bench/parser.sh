4b94c3fe4673caf6a3b429a9a70fb7688b95f6c8
Moisa <radumois@uc81f66006efa52cae38c.ant.amazon.com>
2014-07-04 15:13:34 +0300

    added mapping for hosts

--- ossl_bench/parser.sh
+++ ossl_bench/parser.sh
@@ -3,17 +3,20 @@
 if [[ -d $1 ]]; then
 	dir=${1%/}
 else
-	echo "usage: parser.sh <host files dir>"
+	echo "usage: parser.sh <host files dir> <host mapping file>"
 	exit
 fi
 
 regex="^([-\.a-z0-9]+)\s+([0-9]+)\s+(blowfish cbc|rsa 4096 bits)\s+(.+)"
 #regex="^([-\.a-z0-9]+)\s+([0-9]+)\s+(blowfish cbc)\s+(.+)"
+mapping_regex="([-\.a-zA-Z0-9]+),([-\.a-zA-Z0-9 ]+)"
 
 declare -A hD1
 declare -A hD2
 declare -A hD10
 declare -A hD100
+
+declare -A mapping
 
 for f in $(ls ${dir}/*.txt); do
 #	echo "got file: ${f}"
@@ -37,20 +40,35 @@ for f in $(ls ${dir}/*.txt); do
 	done < "${f}"
 done
 
+if [[ -f "$2" ]]; then
+	while read l; do
+		if [[ $l =~ $mapping_regex ]]; then
+			h=${BASH_REMATCH[1]}
+			t=${BASH_REMATCH[2]}
+#			echo "$h: $t"
+			mapping["$h"]="$t"
+		fi
+	done < "$2"
+fi
+
 echo "--- 1 ---"
 for h in "${!hD1[@]}"; do
-	echo "${h} ${hD1[${h}]}"
+#	echo "${h} ${hD1[${h}]} ${mapping[${hD1[${h}]}]}"
+	echo "${h} ${mapping[${hD1[${h}]}]}"
 done | sort -n -k 3
 echo "--- 2 ---"
 for h in "${!hD2[@]}"; do
-	echo "${h} ${hD2[${h}]}"
+#	echo "${h} ${hD2[${h}]} ${mapping[${hD2[${h}]}]}"
+	echo "${h} ${mapping[${hD2[${h}]}]}"
 done | sort -n -k 3
 echo "--- 10 ---"
 for h in "${!hD10[@]}"; do
-	echo "${h} ${hD10[${h}]}"
+#	echo "${h} ${hD10[${h}]} ${mapping[${hD10[${h}]}]}"
+	echo "${h} ${mapping[${hD10[${h}]}]}"
 done | sort -n -k 3
 echo "--- 100 ---"
 for h in "${!hD100[@]}"; do
-	echo "${h} ${hD100[${h}]}"
+#	echo "${h} ${hD100[${h}]} ${mapping[${hD100[${h}]}]}"
+	echo "${h} $${mapping[${hD100[${h}]}]}"
 done | sort -n -k 3
 
