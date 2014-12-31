e05250f09c21b1a5ebff51da72e1fd19e3738ddf
Radu Moisa <rmoisa@yahoo.com>
2014-12-31 10:00:52 +0200

    added json

--- python/0mq/cl_rep.py
+++ python/0mq/cl_rep.py
@@ -5,8 +5,11 @@ import time
 import zmqclient
 
 cl = zmqclient.client(zmq.REP,'tcp://127.0.0.1:5560')
+print("Started responder {}".format(cl.data['id']))
+cl.set_msg('Hello from backend!')
 while True:
-	print(cl.recv_msg())
-	cl.send_msg("Hello!")
+	d = cl.recv_jsn()
+	print("{}# Client {}: {}".format(cl.data['id'], d['id'], d['msg']))
 	time.sleep(1)
+	cl.send_jsn()
 
--- python/0mq/cl_req.py
+++ python/0mq/cl_req.py
@@ -5,8 +5,11 @@ import time
 import zmqclient
 
 cl = zmqclient.client(zmq.REQ,'tcp://127.0.0.1:5559')
+print("Started requester {}".format(cl.data['id']))
+cl.set_msg('Hello from frontend!')
 while True:
-	cl.send_msg("Hello!")
+	cl.send_jsn()
 	time.sleep(1)
-	print(cl.recv_msg())
+	d = cl.recv_jsn()
+	print("{}# Client {}: {}".format(cl.data['id'], d['id'], d['msg']))
 
