ddebe8d66fbf78742985f1c7c571dbaccf1b0c35
Moisa <radumois@uc81f66006efa52cae38c.ant.amazon.com>
2015-01-22 19:16:30 +0200

    more pipe stuff

--- bash_perl_oneliner.sh
+++ bash_perl_oneliner.sh
@@ -3,4 +3,5 @@ ps|xargs -P2 -i -t bash -c 'echo {}|perl -ne "print $_ if (/zsh/);"'
 echo "HTTP_USER_AGENT=mama"|perl -nle '/(HTTP)/;print $1;'
 echo "HTTP_USER_AGENT=mama"|xargs -P2 -t -i -n1 bash -c 'echo {}|perl -nle "/(HTTP)/;print \$1;"'
 echo "HTTP_USER_AGENT=mama"|xargs -P2 -i -n1 bash -c 'echo "{}"|perl -nle "if (/^HTTP_USER_AGENT=(.+)/){print \$1;}"'
-
+zcat xxx|xargs -P2 -i -n1 bash -c 'echo "{}"|perl -nle "if (/^HTTP_USER_AGENT=(.+)/){print \$1;}"'
+ls xxx|xargs -P8 -i -n1 bash -c 'zcat {}|perl -lne "if (/GET (.+)/){print \$1;}"' | perl -e 'while (<>){$lines{$_}++;}; for (keys %lines){print "$lines{$_} $_";}'
