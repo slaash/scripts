4332f89f7385ab1a5229c8a3300716c1c55373ef
Moisa <radumois@uc81f66006efa52cae38c.ant.amazon.com>
2015-01-22 12:47:51 +0200

    smth

--- bash_perl_oneliner.sh
+++ bash_perl_oneliner.sh
@@ -1,1 +1,6 @@
 zcat access.log.*.gz|perl -lne 'print $1 if ($_ =~ /.+\s.+\s.+\s\[.+\]\s\".+\"\s.+\s.+\s.+\"(.+)\"$/)'|sort|uniq -c |sort -n
+ps|xargs -P2 -i -t bash -c 'echo {}|perl -ne "print $_ if (/zsh/);"'
+echo "HTTP_USER_AGENT=mama"|perl -nle '/(HTTP)/;print $1;'
+echo "HTTP_USER_AGENT=mama"|xargs -P2 -t -i -n1 bash -c 'echo {}|perl -nle "/(HTTP)/;print \$1;"'
+echo "HTTP_USER_AGENT=mama"|xargs -P2 -i -n1 bash -c 'echo "{}"|perl -nle "if (/^HTTP_USER_AGENT=(.+)/){print \$1;}"'
+
