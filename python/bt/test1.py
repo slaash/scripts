6f82c7f22a021e70d0a498bae52ae049c2e3c28e
Radu Moisa <rmoisa@yahoo.com>
2015-02-19 15:12:49 +0200

    aws info

--- python/bt/test1.py
+++ python/bt/test1.py
@@ -2,11 +2,13 @@
 
 import boto.ec2
 
-iam = boto.connect_iam()
+prof_name = 'ec2-user'
+
+iam = boto.connect_iam(profile_name = prof_name)
 user = iam.get_user()
 print("Running as {} ({})".format(user.user_name, user.user_id))
 
-conn = boto.ec2.connect_to_region("eu-central-1")
+conn = boto.ec2.connect_to_region("eu-central-1", profile_name = prof_name)
 reservations = conn.get_all_instances()
 for r in reservations:
 	for i in r.instances:
