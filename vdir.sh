318c70e870dec25df51145e6c1ce379c50e6c432
Radu Moisa <rmoisa@yahoo.com>
2015-05-13 17:03:32 +0300

    fixed

--- vdir.sh
+++ vdir.sh
@@ -5,16 +5,16 @@ dir=${2}
 
 if [[ ${mode} == "size" ]]; then
     echo "Sorted by size"
-    p_mode="%s %h\n"
+    p_mode="%s %p\n"
 else
     if [[ ${mode} == "time" ]];then
         echo "Sorted by time"
-        p_mode="%TY-%Tm-%TdT%TH:%TM %h\n"
+        p_mode="%TY-%Tm-%TdT%TH:%TM %p\n"
     else
         echo "usage: vdir.sh <size|time> <dir>"
         exit
     fi
 fi
 
-find ${dir} -type f -printf "${p_mode}" 2>/dev/null|sort -k 1 -n
+find "${dir}" -type f -printf "${p_mode}" 2>/dev/null|sort -k 1 -n
 
