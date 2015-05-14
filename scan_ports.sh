5ca3df604a3a9bdfd7f3f151222c5e639cb77653
Radu Moisa <rmoisa@yahoo.com>
2015-05-14 12:13:47 +0300

    scan ports into sqlite (+kill by ctrl-c)

---
+++ scan_ports.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "Goodbye!"
+	pkill -f scan_ports.sh
+}
+
+dbf=~/ports.sqlite3
+
+find /usr/ports -maxdepth 2 -type d|while read line; do
+    folder=$(echo "${line}"|cut -d '/' -f 4,5)
+    name=$(echo "${line}"|cut -d '/' -f 5)
+    if ! [[ ${name} == '' ]]; then
+        echo "${name} ${folder}"
+        count=$(sqlite3 ${dbf} "select count(id) from ports where name=\"${name}\"")
+        if [[ ${count} == 0 ]]; then
+            sqlite3 ${dbf} "insert into ports values(NULL, \"${folder}\", \"${name}\")"
+        fi
+    fi
+done
+
