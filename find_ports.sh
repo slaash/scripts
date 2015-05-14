e2b554d42228e3bdf3959cdc8a46da23e9f1f202
Radu Moisa <rmoisa@yahoo.com>
2015-05-14 12:32:50 +0300

    smarter search

--- find_ports.sh
+++ find_ports.sh
@@ -9,7 +9,16 @@ function ctrl_c() {
 
 dbf=~/ports.sqlite3
 
-search=${1}
-
-sqlite3 ${dbf} "select folder from ports where name like \"%${search}%\""
+if [[ $# == 1 ]]; then
+    #basic package search
+    search=${1}
+    sqlite3 ${dbf} "select folder from ports where name like \"%${search}%\""
+else
+    mode=${1}
+    search=${2}
+    if [[ ${mode} == "all" ]]; then
+        #show all packages in group folder
+        sqlite3 ${dbf} "select name from ports where folder like \"${search}/%\""
+    fi
+fi
 
