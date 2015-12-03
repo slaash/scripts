bfa7f4c0874e213842ad72169c103c86203bf7fc
Radu Moisa <rmoisa@yahoo.com>
2015-12-03 14:51:47 +0200

    rsync repo size

--- rsync_mirror.sh
+++ rsync_mirror.sh
@@ -2,3 +2,5 @@
 
 rsync --list-only --dry-run --archive --verbose --delete --delete-delay --delay-updates archive.raspbian.org::archive/raspbian ./
 
+rsync --list-only --dry-run --archive --verbose --delete --delete-delay --delay-updates archive.raspbian.org::archive/raspbian ./|grep "\-\-"|tr -s ' ' |cut -d ' ' -f 2|tr -d ','|paste -sd+ - | bc
+
