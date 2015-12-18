c61140c2dd066123ca0928b0843d7f9c016c0c8d
Radu Moisa <rmoisa@yahoo.com>
2015-12-18 12:34:07 +0200

    nice file renaming!

---
+++ rename_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for file in *; do
+    new_file=${file// /_}
+    if ! [[ "${file}" == "${new_file}" ]]; then
+        mv "${file}" "${new_file}"
+        if [[ $? == 0 ]]; then
+            echo "Renamed ${file} to ${new_file}"
+        fi
+    fi
+done
+
