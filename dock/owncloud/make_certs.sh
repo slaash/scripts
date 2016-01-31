413cd07fa631384f9de322c41dc4ecfa68736951
Radu Moisa <rmoisa@yahoo.com>
2016-01-31 09:16:12 +0200

    fixed owncloud

--- dock/owncloud/make_certs.sh
+++ dock/owncloud/make_certs.sh
@@ -1,4 +1,6 @@
-#!?bin/bash
+#!/bin/bash
+
+cd ~/ssl/
 
 openssl genrsa -des3 -out server.key 1024 && openssl rsa -in server.key -out server.key.insecurexx && openssl req -new -key server.key -out server.csr && openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 
