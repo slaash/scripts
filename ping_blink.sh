0a7fdb60d109ba6fc0db204032c3c40e9a898bb4
Radu Moisa <rmoisa@yahoo.com>
2016-07-24 09:28:33 +0300

    added ping exit codes

--- ping_blink.sh
+++ ping_blink.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IP=${1}
+
 set -e
 
 function set_trigger {
@@ -7,10 +9,6 @@ function set_trigger {
     old_trigger=$(grep -oE "\[.+\]" /sys/class/leds/led0/trigger|sed -r "s/(\[|\])//g")
     sudo sh -c 'echo "none">/sys/class/leds/led0/trigger'
     echo "${old_trigger}"
-}
-
-function reset_trigger {
-    sudo sh -c "echo \"${1}\">/sys/class/leds/led0/trigger"
 }
 
 function light {
@@ -22,30 +20,27 @@ function dark {
 }
 
 function ping_host {
-    ping -c 1 "${1}" >/dev/null 2>&1
+    ping -c 1 -W 1 "${1}" >/dev/null 2>&1
     echo $?
 }
 
-old_t=$(set_trigger)
+set_trigger
 dark
 
 while true ; do
-    ret=$(ping_host 192.168.0.100)
-    if [[ $ret == 0 ]]; then
-        light
-        sleep 1
-    else
-        light
-        sleep 0.1
-        dark
-        sleep 0.1
-        light
-        sleep 0.1
+    ret=$(ping_host "${IP}")
+    echo "${ret}"
+    # hosts responding
+    if [[ ${ret} == 0 ]]; then
+        light;sleep 1;dark;sleep 1
+    # host not responding
+    elif [[ ${ret} == 1 ]]; then
+        light;sleep 0.1;dark;sleep 0.1
+        light;sleep 0.1;dark
+    # host does not exist
+    elif [[ ${ret} == 2 ]]; then
+        light;sleep 0.1;dark;sleep 0.1
+        light;sleep 0.1;dark;sleep 0.1
+        light;sleep 0.1;dark;sleep 1
     fi
-    dark
-    sleep 1
 done
-
-echo "Resetting trigger from none to ${old_t}"
-reset_trigger "${old_t}"
-
