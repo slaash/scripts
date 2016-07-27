8daed93e844006e77926fe8b20898188652ccafe
Radu Moisa <rmoisa@yahoo.com>
2016-07-27 17:11:51 +0300

    human readable values

---
+++ python/csv_to_human.py
@@ -0,0 +1,11 @@
+import csv
+import sys, os, datetime
+
+if os.path.isfile(sys.argv[1]):
+    csv_file = sys.argv[1]
+    with open(csv_file) as inputFile:
+        inputCSV = csv.reader(inputFile, delimiter=',')
+        for row in inputCSV:
+            time, val = row
+            print("{}: {} MB".format(datetime.datetime.fromtimestamp(float(time)).strftime('%Y-%m-%d %H:%M:%S'), int(float(val)/1024/1024)))
+
