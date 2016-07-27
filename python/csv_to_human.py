bf577ea87c4657d5a5506f0c14e4060140ab918c
Radu Moisa <rmoisa@yahoo.com>
2016-07-27 17:16:43 +0300

    removed first line

--- python/csv_to_human.py
+++ python/csv_to_human.py
@@ -4,6 +4,7 @@ import sys, os, datetime
 if os.path.isfile(sys.argv[1]):
     csv_file = sys.argv[1]
     with open(csv_file) as inputFile:
+        next(inputFile)
         inputCSV = csv.reader(inputFile, delimiter=',')
         for row in inputCSV:
             time, val = row
