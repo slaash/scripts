748f32226c4cf70c964fce7ea9b613d6bcae3f26
Radu Moisa <rmoisa@yahoo.com>
2016-08-20 15:17:39 +0300

    fixed mouse events

--- python/morsepi/ev2.py
+++ python/morsepi/ev2.py
@@ -6,33 +6,26 @@ print(dev)
 SENSIV = 10
 
 x = 0
-c_x = 0
 y = 0
-c_y = 0
 c = 0
 for event in dev.read_loop():
     if event.type == ecodes.EV_REL:
-#       print(event.code)
         c = c+1
         if event.code == ecodes.ecodes['REL_X']:
-            c_x = c_x+1
             x = x+event.value
         elif event.code == ecodes.ecodes['REL_Y']:
-            c_y = c_y+1
             y = y+event.value
         if c % SENSIV == 0:
-            if c_x > c_y:
+            if abs(x) > abs(y):
                 if x > 0:
-                    print('>')
-                elif x < 0:
-                    print('<')
-            elif c_x < c_y:
+                    print('>'.format(x,y))
+                elif x <= 0:
+                    print('<'.format(x,y))
+            elif abs(x) <= abs(y):
                 if y > 0:
-                    print(r'\/')
-                elif y < 0:
-                    print('^')
+                    print(r'\/'.format(x,y))
+                elif y <= 0:
+                    print('^'.format(x,y))
             x = 0
-            c_x = 0
             y = 0
-            c_y = 0
 
