c137bc3bb6e5d7e4b2ac3ba73042a18c90079c23
Radu Moisa <rmoisa@yahoo.com>
2016-08-20 16:35:13 +0300

    added time, some cleanup

--- python/morsepi/morsepi.py
+++ python/morsepi/morsepi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import readchar
-import subprocess
-from time import sleep
+from readchar import readkey
+from subprocess import call
+from time import sleep, strftime, localtime
 from evdev import InputDevice, categorize, ecodes
 import sys
 
@@ -11,118 +11,146 @@ MODE = "local"
 DEBUG = True
 
 letters = {'a': ['.', '-'],
-            'b': ['-', '.', '.', '.'],
-            'c': ['-', '.', '-', '.'],
-            'd': ['-', '.', '.'],
-            'e': ['.'],
-            'f': ['.', '.', '-', '.'],
-            'g': ['-', '-', '.'],
-            'h': ['.', '.', '.', '.'],
-            'i': ['.', '.'],
-            'j': ['.', '-', '-', '-'],
-            'k': ['-', '.', '-'],
-            'l': ['.', '-', '.', '.'],
-            'm': ['-', '-'],
-            'n': ['-', '.'],
-            'o': ['-', '-', '-'],
-            'p': ['.', '-', '-', '.'],
-            'q': ['-', '-', '.', '-'],
-            'r': ['.', '-', '.'],
-            's': ['.', '.', '.'],
-            't': ['-'],
-            'u': ['.', '.', '-'],
-            'v': ['.', '.', '.', '-'],
-            'w': ['.', '-', '-'],
-            'x': ['-', '.', '.', '-'],
-            'y': ['-', '.', '-', '-'],
-            'z': ['-', '-', '.', '.'],
-            '0': ['-', '-', '-', '-', '-'],
-            '1': ['.', '-', '-', '-', '-'],
-            '2': ['.', '.', '-', '-', '-'],
-            '3': ['.', '.', '.', '-', '-'],
-            '4': ['.', '.', '.', '.', '-'],
-            '5': ['.', '.', '.', '.', '.'],
-            '6': ['-', '.', '.', '.', '.'],
-            '7': ['-', '-', '.', '.', '.'],
-            '8': ['-', '-', '-', '.', ','],
-            '9': ['-', '-', '-', '-', '.'],
-            '-': ['-', '.', '.', '.', '.', '-'],
-            '+': ['.', '-', '.', '-', '.']}
+           'b': ['-', '.', '.', '.'],
+           'c': ['-', '.', '-', '.'],
+           'd': ['-', '.', '.'],
+           'e': ['.'],
+           'f': ['.', '.', '-', '.'],
+           'g': ['-', '-', '.'],
+           'h': ['.', '.', '.', '.'],
+           'i': ['.', '.'],
+           'j': ['.', '-', '-', '-'],
+           'k': ['-', '.', '-'],
+           'l': ['.', '-', '.', '.'],
+           'm': ['-', '-'],
+           'n': ['-', '.'],
+           'o': ['-', '-', '-'],
+           'p': ['.', '-', '-', '.'],
+           'q': ['-', '-', '.', '-'],
+           'r': ['.', '-', '.'],
+           's': ['.', '.', '.'],
+           't': ['-'],
+           'u': ['.', '.', '-'],
+           'v': ['.', '.', '.', '-'],
+           'w': ['.', '-', '-'],
+           'x': ['-', '.', '.', '-'],
+           'y': ['-', '.', '-', '-'],
+           'z': ['-', '-', '.', '.'],
+           '0': ['-', '-', '-', '-', '-'],
+           '1': ['.', '-', '-', '-', '-'],
+           '2': ['.', '.', '-', '-', '-'],
+           '3': ['.', '.', '.', '-', '-'],
+           '4': ['.', '.', '.', '.', '-'],
+           '5': ['.', '.', '.', '.', '.'],
+           '6': ['-', '.', '.', '.', '.'],
+           '7': ['-', '-', '.', '.', '.'],
+           '8': ['-', '-', '-', '.', ','],
+           '9': ['-', '-', '-', '-', '.'],
+           '-': ['-', '.', '.', '.', '.', '-'],
+           '+': ['.', '-', '.', '-', '.']}
 
 ecodez = {'KEY_A': 'a', 'KEY_B': 'b', 'KEY_C': 'c', 'KEY_D': 'd', 'KEY_E': 'e', 'KEY_F': 'f',
-           'KEY_G': 'g', 'KEY_H': 'h', 'KEY_I': 'i', 'KEY_J': 'j', 'KEY_K': 'k', 'KEY_L': 'l',
-           'KEY_M': 'm', 'KEY_N': 'n', 'KEY_O': 'o', 'KEY_P': 'p', 'KEY_Q': 'q', 'KEY_R': 'r',
-           'KEY_S': 's', 'KEY_T': 't', 'KEY_U': 'u', 'KEY_V': 'v', 'KEY_W': 'w', 'KEY_X': 'x',
-           'KEY_Y': 'y', 'KEY_Z': 'z',
-           'KEY_0': '0', 'KEY_1': '1', 'KEY_2': '2', 'KEY_3':'3', 'KEY_4': '4', 'KEY_5': '5',
-           'KEY_6': '6', 'KEY_7': '7', 'KEY_8': '8', 'KEY_9': '9'}
+          'KEY_G': 'g', 'KEY_H': 'h', 'KEY_I': 'i', 'KEY_J': 'j', 'KEY_K': 'k', 'KEY_L': 'l',
+          'KEY_M': 'm', 'KEY_N': 'n', 'KEY_O': 'o', 'KEY_P': 'p', 'KEY_Q': 'q', 'KEY_R': 'r',
+          'KEY_S': 's', 'KEY_T': 't', 'KEY_U': 'u', 'KEY_V': 'v', 'KEY_W': 'w', 'KEY_X': 'x',
+          'KEY_Y': 'y', 'KEY_Z': 'z',
+          'KEY_0': '0', 'KEY_1': '1', 'KEY_2': '2', 'KEY_3':'3', 'KEY_4': '4', 'KEY_5': '5',
+          'KEY_6': '6', 'KEY_7': '7', 'KEY_8': '8', 'KEY_9': '9'}
 
 def set_trigger():
-    subprocess.call(['sudo', 'sh', '-c', "echo \"none\">\"/sys/class/leds/led0/trigger\""])
+    call(['sudo', 'sh', '-c', "echo \"none\">\"/sys/class/leds/led0/trigger\""])
 
 def light():
-    subprocess.call(['sudo', 'sh', '-c', "echo \"255\">\"/sys/class/leds/led0/brightness\""])
+    call(['sudo', 'sh', '-c', "echo \"255\">\"/sys/class/leds/led0/brightness\""])
 
 def dark():
-    subprocess.call(['sudo', 'sh', '-c', "echo \"0\">\"/sys/class/leds/led0/brightness\""])
+    call(['sudo', 'sh', '-c', "echo \"0\">\"/sys/class/leds/led0/brightness\""])
+
+def blink(t):
+    light()
+    sleep(t)
+    dark()
 
 def power_off():
-    subprocess.call(['sudo', 'poweroff'])
+    call(['sudo', 'poweroff'])
+
+#if the duration of a dot is taken to be one unit
+#then that of a dash is three units.
+#The space between the components of one character is one unit,
+#between characters is three units and between words seven units.
+#To indicate that a mistake has been made and for the receiver to delete the last word, send ........ (eight dots).
 
 def show_letter(l):
-    if DEBUG:
-        sys.stdout.write("{} ( {} ) ... ".format(l, " ".join(letters[l])))
-        sys.stdout.flush()
-    for c in letters[l]:
-        if c == '.':
-            light()
-            sleep(UNIT)
-        elif c == '-':
-            light()
-            sleep(3*UNIT)
-        dark()
-        sleep(UNIT)
-    if DEBUG:
-        print("done.")
+    l = l.lower()
+    if l in letters:
+        if DEBUG:
+            sys.stdout.write("{} ( {} ) ... ".format(l, " ".join(letters[l])))
+            sys.stdout.flush()
+        c = 0
+        while c < len(letters[l]):
+            if letters[l][c] == '.':
+                blink(UNIT)
+            elif letters[l][c] == '-':
+                blink(3*UNIT)
+            if c < len(letters[l])-1:
+                dark()
+                sleep(UNIT)
+            c = c+1
+        if DEBUG:
+            print("done.")
+        return 0
+    else:
+        return 1
 
 def show_word(w):
     if DEBUG:
         print(w)
-    for l in w:
-        show_letter(l)
-        sleep(3*UNIT)
+    c = 0
+    while c < len(w):
+        show_letter(w[c])
+        if c < len(w)-1:
+            sleep(3*UNIT)
+        c = c+1
+    sleep(7*UNIT)
 
-set_trigger()
-dark()
-secv = ""
-print("Started")
-if MODE == "local":
-    while True:
-        key = readchar.readkey()
-        if ord(key) == 13:
-            show_word(secv)
-            secv = ""
-        else:
-            show_letter(key)
-            secv = secv + key
-else:
-    dev = InputDevice('/dev/input/event0')
-    if DEBUG:
-        print(dev)
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            c = categorize(event)
-            if c.keystate == c.key_down:
-                if event.code == ecodes.ecodes['KEY_ENTER']:
-                    if secv == 'bye':
-                        power_off()
+def process_secv(s):
+    if secv == 'bye':
+        power_off()
+    elif secv == 'time':
+        for w in strftime("%I %M %p", localtime()).split(" "):
+            show_word(w)
+    else:
+        show_word(s)
+
+if __name__ == "__main__":
+    set_trigger()
+    dark()
+    secv = ""
+    print("Started")
+    if MODE == "local":
+        while True:
+            key = readkey()
+            if ord(key) == 13:
+                process_secv(secv)
+                secv = ""
+            else:
+                if show_letter(key) == 0:
+                    secv = secv + key
+    else:
+        dev = InputDevice('/dev/input/event0')
+        if DEBUG:
+            print(dev)
+        for event in dev.read_loop():
+            if event.type == ecodes.EV_KEY:
+                c = categorize(event)
+                if c.keystate == c.key_down:
+                    if event.code == ecodes.ecodes['KEY_ENTER']:
+                        process_secv(secv)
+                        secv = ""
                     else:
-                        show_word(secv)
-                    secv = ""
-                else:
-                    for k in ecodez.keys():
-                        if event.code == ecodes.ecodes[k]:
-                            letter = ecodez[k]
-                            show_letter(letter)
-                            secv = secv + letter
+                        for k in ecodez.keys():
+                            if event.code == ecodes.ecodes[k]:
+                                letter = ecodez[k]
+                                show_letter(letter)
+                                secv = secv + letter
 
