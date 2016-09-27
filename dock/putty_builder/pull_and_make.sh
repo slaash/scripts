b5d8fa1d6b583fd6db2e24859dd0c2dfc6d11909
Radu Moisa <rmoisa@yahoo.com>
2016-09-27 09:32:20 +0300

    updated putty builder to use latest version and mingw compiler

--- dock/putty_builder/pull_and_make.sh
+++ dock/putty_builder/pull_and_make.sh
@@ -2,11 +2,11 @@
 
 cd /opt/putty
 git pull
-git fetch --tags
-git checkout $(git tag|sort -rn|head -1)
-sed -i 's/-mno-cygwin//g' ./mkfiles.pl
+#git fetch --tags
+#git checkout $(git tag|sort -rn|head -1)
+#sed -i 's/-mno-cygwin//g' ./mkfiles.pl
 perl mkfiles.pl
-make -C windows -f Makefile.cyg clean
-make -C windows XFLAGS=-DCOVERITY VER="-DSNAPSHOT=$(date '+%Y-%m-%d') -DSVN_REV=$(cat LATEST.VER) -DMODIFIED" TOOLPATH=i686-w64-mingw32- -f Makefile.cyg
+make -C windows -f Makefile.mgw clean
+make -C windows XFLAGS=-DCOVERITY VER="-DSNAPSHOT=$(date '+%Y-%m-%d') -DSVN_REV=$(cat LATEST.VER) -DMODIFIED" TOOLPATH=i686-w64-mingw32- -f Makefile.mgw
 /opt/copy_files.sh
 
