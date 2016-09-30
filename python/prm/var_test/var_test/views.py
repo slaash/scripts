2bb9a3789a3c3b1a8abb62c7f5c05866db99c037
Radu Moisa <rmoisa@yahoo.com>
2016-09-30 18:12:45 +0300

    pyramid json listener

--- python/prm/var_test/var_test/views.py
+++ python/prm/var_test/var_test/views.py
@@ -1,6 +1,12 @@
 from pyramid.view import view_config
-
+from pyramid.response import Response
+import json
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'var_test'}
+
+@view_config(route_name='event')
+def getDockerEvent(request):
+    print(request.body.decode("utf-8"))
+    return Response(status=200)
