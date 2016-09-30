f6066367f6e92acc132a8cdd6f48879ac46237b8
Radu Moisa <rmoisa@yahoo.com>
2016-09-30 18:12:45 +0300

    pyramid json listener

--- python/prm/var_test/var_test/__init__.py
+++ python/prm/var_test/var_test/__init__.py
@@ -13,5 +13,6 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('event', '/event')
     config.scan()
     return config.make_wsgi_app()
