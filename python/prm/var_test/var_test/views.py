a74fe43ddb1bc5bd7a2c59c66669786de8a55aff
Radu Moisa <rmoisa@yahoo.com>
2016-10-01 16:45:02 +0300

    logging, testing

--- python/prm/var_test/var_test/views.py
+++ python/prm/var_test/var_test/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 import logging
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,5 +12,17 @@ def my_view(request):
 
 @view_config(route_name='event')
 def getDockerEvent(request):
-    logger.info(request.body.decode("utf-8"))
+    body = request.body.decode("utf-8")
+    logger.info(body)
+    try:
+        data = json.loads(body)
+    except ValueError as err:
+        logger.info(err)
+        return Response(status='404')
+    if data['events'][0]['action'] == 'push':
+        time = data['events'][0]['timestamp']
+        repo = data['events'][0]['target']['repository']
+        tag = data['events'][0]['target']['tag']
+        name = data['events'][0]['actor']['name']
+        logger.info("{}: {} pushed {}:{}".format(time, name, repo, tag))
     return Response(status=200)
