7811fa96a39a99b08884f5028977c923269585df
Radu Moisa <rmoisa@yahoo.com>
2016-10-03 17:11:19 +0300

    added scan action, smtp, docker, go, clair cli tool

--- python/prm/var_test/var_test/__init__.py
+++ python/prm/var_test/var_test/__init__.py
@@ -18,5 +18,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('event', '/event')
+    config.add_route('scan', '/scan/{repo}/{tag}/{user}')
     config.scan()
     return config.make_wsgi_app()
