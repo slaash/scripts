eef7ddd6e7d938bb4ac5887c845c1703e18a04bf
Radu Moisa <rmoisa@yahoo.com>
2016-11-26 18:02:35 +0200

    fixed owncloud

--- dock/owncloud/make_certs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/ssl/
-
-openssl genrsa -des3 -out server.key 1024 && openssl rsa -in server.key -out server.key.insecurexx && openssl req -new -key server.key -out server.csr && openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-
--- dock/owncloud/start_apache.sh
+++ dock/owncloud/start_apache.sh
@@ -4,4 +4,3 @@ export APACHE_CONFDIR=/etc/apache2
 source $APACHE_CONFDIR/envvars
 
 /usr/sbin/apache2 -k start -e info -DFOREGROUND
-
