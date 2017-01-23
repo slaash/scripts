9f8c176be0eb14844792d7a7159097ed212410b5
Radu Moisa <rmoisa@yahoo.com>
2017-01-23 13:21:25 +0200

    added used swap

--- sysinfo_awk.sh
+++ sysinfo_awk.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-awk 'BEGIN { printf "%-19s %-23s %-15s\n", "Date", "Free Memory Free Swap", "System Load" }'
+awk 'BEGIN { printf "%-19s %-16s %-14s %-14s %s\n", "Date", "Free Memory (kB)", "Used Swap (kB)", "Free Swap (kB)", "System Load" }'
 
 while true;
 do
     meminfo=$(awk '
-                   tolower($1) ~ /memfree/ { memfree = $2" "$3 }
-                   tolower($1) ~ /swapfree/ { swapfree = $2" "$3 }
-                   END { printf "%-11s %-11s", memfree, swapfree }' /proc/meminfo)
+                   tolower($1) ~ /memfree/ { memfree = $2 }
+                   tolower($1) ~ /swapfree/ { swapfree = $2 }
+                   tolower($1) ~ /swaptotal/ { swaptotal = $2 }
+                   END { printf "%-16s %-14s %-14s", memfree, swaptotal-swapfree, swapfree }' /proc/meminfo)
     echo "$(date +'%Y/%m/%d %H:%M:%S') ${meminfo} $(cut -d ' ' -f 1,2,3 /proc/loadavg)"
     sleep 5
 done
