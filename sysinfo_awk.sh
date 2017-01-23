4987d4ce21c1450ba772031d91647e2a160a437e
Radu Moisa <rmoisa@yahoo.com>
2017-01-23 16:45:57 +0200

    finally, bloody thousand separators in awk!

--- sysinfo_awk.sh
+++ sysinfo_awk.sh
@@ -8,7 +8,7 @@ do
                    tolower($1) ~ /memfree/ { memfree = $2 }
                    tolower($1) ~ /swapfree/ { swapfree = $2 }
                    tolower($1) ~ /swaptotal/ { swaptotal = $2 }
-                   END { printf "%-16s %-14s %-14s", memfree, swaptotal-swapfree, swapfree }' /proc/meminfo)
+                   END { printf "%'"'"'-16d %'"'"'-14d %'"'"'-14d", memfree, swaptotal-swapfree, swapfree }' /proc/meminfo)
     echo "$(date +'%Y/%m/%d %H:%M:%S') ${meminfo} $(cut -d ' ' -f 1,2,3 /proc/loadavg)"
     sleep 5
 done
