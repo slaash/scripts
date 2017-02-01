05e8010d1bdecfbc0216688fe4ee5d9dc846f08a
Radu Moisa <rmoisa@yahoo.com>
2017-02-01 14:57:04 +0200

    added smart trap, started pater

--- python/pater/pater.py
+++ python/pater/pater.py
@@ -1,9 +1,19 @@
 from multiprocessing import Process
+import psutil
+import time
 
 def _procWatcher():
     while True:
-        
+        for pid in psutil.pids():
+            p = psutil.Process(pid)
+            try:
+                print(p.name(), p.exe())
+                if p.name == 'top':
+                    print 
+            except Exception:
+                pass
+        time.sleep(5)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     watch = Process(target=_procWatcher)
     watch.start()
