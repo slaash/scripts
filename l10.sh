0b3d3a34bf10855f18f3843a3b3267ad98f1717c
Radu Moisa <rmoisa@yahoo.com>
2017-04-25 15:43:28 +0300

    added find

--- l10.sh
+++ l10.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Show all files EXCEPT the newest 10
 
-for i in $(ls -t file-*|tail -n +10);do
+echo "--- ls ---"
+for i in $(ls -t file-*|tail -n +11);do
     echo "${i}"
 done
 
+echo "--- find ---"
+for i in $(find . -mindepth 1 -maxdepth 1 -type f|sort -r|tail -n +11); do
+    echo "${i}"
+done
