b6cec1d668a19dea56ca25d84bde5239cd85fcdd
Radu Moisa <rmoisa@yahoo.com>
2017-06-05 14:49:06 +0300

    added wind speed

--- weather_hist_temp.sh
+++ weather_hist_temp.sh
@@ -2,4 +2,4 @@
 
 start=$1
 
-curl -s "https://www.wunderground.com/history/airport/LRIA/${1}/CustomHistory.html?dayend=$(date +%d)&monthend=$(date +%m)&yearend=$(date +%Y)&req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo=&format=1"|cut -d "," -f 1,2,3,4
+curl -s "https://www.wunderground.com/history/airport/LRIA/${1}/CustomHistory.html?dayend=$(date +%d)&monthend=$(date +%m)&yearend=$(date +%Y)&req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo=&format=1"|awk -F ',' '{ print $1,"\t",$2,"\t",$3,"\t",$4,"\t",$17,"\t",$18 }'
--- weather_today_temp.sh
+++ weather_today_temp.sh
@@ -3,5 +3,5 @@
 today=$(date +%Y/%m/%d)
 echo ${today}
 
-curl -s "https://www.wunderground.com/history/airport/LRIA/${today}/DailyHistory.html?format=1"|cut -d "," -f 1,2
+curl -s "https://www.wunderground.com/history/airport/LRIA/${today}/DailyHistory.html?format=1"|awk -F ',' '{ print $1,"\t",$2,"\t",$8 }'
 
