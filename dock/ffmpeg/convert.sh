4f8b31296a38f7bd1435d2f2f9c945024e3b63dd
Radu Moisa <radu@uberresearch.com>
2017-10-07 16:25:16 +0300

    added convert script

---
+++ dock/ffmpeg/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SRCDIR=${1}
+
+find "${SRCDIR}" -name "*.flac"| while read -r flac;
+do
+	echo "${flac}"
+	dir=$(dirname "${flac}"|sed -e 's/.*\///g')
+	name=$(echo "${flac}"|sed -e 's/.*\///g')
+        mp3=${name//.flac/.mp3}
+	dst="./mp3/${dir}/${mp3}"
+	mkdir -p "./mp3/${dir}"
+	echo "${dst}"
+	/opt/ffmpeg/bin/ffmpeg -y -i "${flac}" -b:a 128k "${dst}" < /dev/null
+done
