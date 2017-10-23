d5e34ab38ac64b4d9778d0a1428f7036d8c73219
Radu Moisa <radu@uberresearch.com>
2017-10-23 17:08:13 +0300

    added mp3 reconvert script

---
+++ dock/ffmpeg/convert_mp3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRCDIR=${1}
+DSTDIR=${2}
+
+find "${SRCDIR}" -name "*.mp3"| while read -r flac;
+do
+	echo "${flac}"
+	dir=$(dirname "${flac}"|sed -e 's/.*\///g')
+	name=$(echo "${flac}"|sed -e 's/.*\///g')
+        mp3=${name//.mp3/.mp3}
+	dst="${DSTDIR}/${dir}/${mp3}"
+        mkdir -p "${DSTDIR}/${dir}"
+	echo "${dst}"
+	/opt/ffmpeg/bin/ffmpeg -y -i "${flac}" -map 0:a -c:a libmp3lame -b:a 128k -id3v2_version 3 "${dst}" < /dev/null
+done
