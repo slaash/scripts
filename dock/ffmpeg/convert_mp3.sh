274e10686059d559957ca442623ab00d29679134
Radu Moisa <radu@uberresearch.com>
2017-11-24 17:05:52 +0200

    added m4a

--- dock/ffmpeg/convert_mp3.sh
+++ dock/ffmpeg/convert_mp3.sh
@@ -3,14 +3,15 @@
 SRCDIR=${1}
 DSTDIR=${2}
 
-find "${SRCDIR}" -name "*.mp3"| while read -r flac;
+find "${SRCDIR}" \( -name "*.mp3" -o -name "*.m4a" \)| while read -r flac;
 do
 	echo "${flac}"
 	dir=$(dirname "${flac}"|sed -e 's/.*\///g')
 	name=$(echo "${flac}"|sed -e 's/.*\///g')
-        mp3=${name//.mp3/.mp3}
+        mp3=$(echo $name|sed 's/...$/mp3/')
 	dst="${DSTDIR}/${dir}/${mp3}"
         mkdir -p "${DSTDIR}/${dir}"
 	echo "${dst}"
 	/opt/ffmpeg/bin/ffmpeg -y -i "${flac}" -map 0:a -c:a libmp3lame -b:a 128k -id3v2_version 3 "${dst}" < /dev/null
 done
+
