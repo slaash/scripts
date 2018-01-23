802e15542e950f9e8fd7c52e6701a2887a27ea10
Radu Moisa <rmoisa@yahoo.com>
2018-01-23 10:15:16 +0200

    added ipv4

--- whoismyip.sh
+++ whoismyip.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 curl -s http://freegeoip.net/csv/|cut -d ',' -f 1|xargs -i -t whois {} |grep "descr"
-
+curl -4s http://freegeoip.net/csv/|cut -d ',' -f 1|xargs -i -t whois {} |grep "descr"
