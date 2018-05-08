1d2cfd92925e983c0d6df30d5ce4530d63bd5afb
Radu Moisa <radu@uberresearch.com>
2018-05-08 12:28:07 +0300

    added colors

--- python/weather.py
+++ python/weather.py
@@ -1,14 +1,15 @@
-from datetime import date
 import json
 import argparse
+from datetime import date
+from termcolor import colored
 import requests
 
 DEBUG = False
-stationList = ['LRIA', 'pws:IIAI8', 'pws:IIASIIAS4']
+stationList = ['zmw:00000.133.15090', 'RO/LRIA', 'RO/pws:IIAI8', 'RO/pws:IIASIIAS4', 'zmw:00000.1.15300']
 station = stationList[0]
 
 def history(day):
-    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/history_{}/lang:RO/q/RO/{}.json'.format(day, station))
+    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/history_{}/lang:RO/q/{}.json'.format(day, station))
     req.raise_for_status()
     if DEBUG:
         print(req.request.path_url)
@@ -24,11 +25,11 @@ def history(day):
         wdire = record['wdire']
         hum = record['hum']
         pressurem = record['pressurem']
-        records.append('{}: Temp {} C, Pressure {} hPa, Wind {} km/h from {}, Humidity {}%, {}'.format(time, round(float(tempm)), pressurem, round(float(wspdm)), wdire, hum, conds))
+        records.append('{}: Temp {} C, Pressure {} hPa, Wind {} km/h from {}, Humidity {} %, {}'.format(colored(time, attrs=['bold']), colored(round(float(tempm)), 'green'), colored(pressurem, 'blue'), colored(round(float(wspdm)), 'yellow'), wdire, colored(hum, 'magenta'), colored(conds, 'cyan')))
     return records
 
 def forecast():
-    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/forecast10day/lang:RO/q/RO/{}.json'.format(station))
+    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/forecast10day/lang:RO/q/{}.json'.format(station))
     req.raise_for_status()
     if DEBUG:
         print(req.request.path_url)
@@ -43,11 +44,11 @@ def forecast():
         maxwind_dir = record['maxwind']['dir']
         avewind = record['avewind']['kph']
         avehumidity = record['avehumidity']
-        records.append('{}: Temp {}/{} C, Wind {}(max {}) km/h from {}, Humidity {}%, {}'.format(time, low, high, avewind, maxwind, maxwind_dir, avehumidity, cond))
+        records.append('{}: Temp {}/{} C, Wind {}(max {}) km/h from {}, Humidity {} %, {}'.format(colored(time, attrs=['bold']), colored(low, 'blue'), colored(high, 'red'), colored(avewind, 'yellow'), colored(maxwind, 'red'), maxwind_dir, colored(avehumidity, 'magenta'), colored(cond, 'cyan')))
     return records
 
 def conditions():
-    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/conditions/lang:RO/q/RO/{}.json'.format(station))
+    req = requests.get('https://api.wunderground.com/api/c7862614119770b4/conditions/lang:RO/q/{}.json'.format(station))
     req.raise_for_status()
     if DEBUG:
         print(req.request.path_url)
@@ -62,7 +63,7 @@ def conditions():
     wind_dir = record['wind_dir']
     pressure_mb = record['pressure_mb']
     feelslike_c = record['feelslike_c']
-    return '{}: Temp {}(feels like {}) C, Wind {} km/h from {} ({}), Pressure {} hPa, Humidity {},  {}'.format(time, round(float(temp_c)), round(float(feelslike_c)), wind_kph, wind_dir, wind_string, pressure_mb, relative_humidity, weather)
+    return '{}: Temp {}(feels like {}) C, Wind {} km/h from {} ({}), Pressure {} hPa, Humidity {},  {}'.format(colored(time, attrs=['bold']), colored(round(float(temp_c)), 'green'), colored(round(float(feelslike_c)), 'red'), colored(wind_kph, 'yellow'), wind_dir, colored(wind_string, 'blue'), colored(pressure_mb, 'blue'), colored(relative_humidity, 'magenta'), colored(weather, 'cyan'))
 
 def astronomy():
     req = requests.get('https://api.wunderground.com/api/c7862614119770b4/astronomy/q/zmw:00000.133.15090.json')
@@ -90,8 +91,6 @@ if __name__ == "__main__":
                         help='Shows historical weather data')
     parser.add_argument('--forecast', '-f', action="store_true",
                         help='Shows a weather summary for the next 3 days')
-    parser.add_argument('--conditions', '-c', action="store_true",
-                        help='Shows the current weather conditions')
     parser.add_argument('--station', '-s', default=stationList[0],
                         help='The weather station (one of {})'.format(stationList))
     parser.add_argument('--debug', '-D', action="store_true",
