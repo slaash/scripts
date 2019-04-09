8decb4dc8dcd69d7c62f1f28afa52a5053844b66
Radu Moisa <radu@uberresearch.com>
2019-04-09 11:15:46 +0300

    added k8s plauground

---
+++ weather_owm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#curl 'https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=3d76982f29ecaf53bfcbb46960616d29&q=rediu,iasi&units=metric'
+curl 'https://api.openweathermap.org/data/2.5/history?APPID=3d76982f29ecaf53bfcbb46960616d29&q=rediu,iasi&units=metric'
