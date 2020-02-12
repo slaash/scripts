4a85d8d6ce22f9744a38c41fa7b9ac3ae716e59f
Radu Moisa <radu@uberresearch.com>
2020-02-12 14:42:09 +0200

    sync old files

--- whoismyip.sh
+++ whoismyip.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-curl -s 'http://api.ipstack.com/89.238.244.126?access_key=18d7fb844058a5a95b84e2f6c6e8d2b0&format=1'|jq -r '.ip'|xargs -i -t whois {} |grep "descr"
-curl -4s 'http://api.ipstack.com/89.238.244.126?access_key=18d7fb844058a5a95b84e2f6c6e8d2b0&format=1'|jq -r '.ip'|xargs -i -t whois {} |grep "descr"
+curl -s 'http://api.ipstack.com/check?access_key=18d7fb844058a5a95b84e2f6c6e8d2b0&format=1'|jq -r '.ip'|xargs -i -t whois {} |grep "descr"
+curl -4s 'http://api.ipstack.com/check?access_key=18d7fb844058a5a95b84e2f6c6e8d2b0&format=1'|jq -r '.ip'|xargs -i -t whois {} |grep "descr"
