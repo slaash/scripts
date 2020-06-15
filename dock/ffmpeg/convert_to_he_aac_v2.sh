22366a725e7beaa2fd9d5adb902e2e1eb7a8ae43
Radu Moisa <radu@uberresearch.com>
2020-06-15 12:17:50 +0300

    updated ffmpeg, added he-aac script

---
+++ dock/ffmpeg/convert_to_he_aac_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRCDIR=${1}
+DSTDIR=${2}
+
+find "${SRCDIR}" \( -iname "*.mp3" -o -iname "*.m4a" -o -iname "*.flac" \)| while read -r origFile;
+do
+    echo "${origFile}"
+    dir=$(dirname "${origFile}"|sed -e 's/.*\///g')
+    name=$(echo "${origFile}"|sed -e 's/.*\///g')
+    m4a=$(echo $name|sed -r 's/\.[^\.]+$/.m4a/')
+    dst="${DSTDIR}/${dir}/${m4a}"
+    mkdir -p "${DSTDIR}/${dir}"
+    echo "${dst}"
+    /opt/ffmpeg/bin/ffmpeg -y -i "${origFile}" -map 0:a -c:a libfdk_aac -profile:a aac_he_v2 -b:a 128k -id3v2_version 3 "${dst}" < /dev/null
+done
