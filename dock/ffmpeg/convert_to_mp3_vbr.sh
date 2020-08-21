054b2f5c6f835d4430ae9b6fd217b97f05ff11b2
Radu Moisa <radu@uberresearch.com>
2020-08-21 16:12:24 +0300

    added vbr mp3 script

---
+++ dock/ffmpeg/convert_to_mp3_vbr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRCDIR=${1}
+DSTDIR=${2}
+
+find "${SRCDIR}" \( -iname "*.mp3" -o -iname "*.m4a" -o -iname "*.flac" \)| while read -r origFile;
+do
+    echo "${origFile}"
+    dir=$(dirname "${origFile}")
+    name=$(echo "${origFile}"|sed -e 's/.*\///g')
+    mp3=$(echo $name|sed -r 's/\.[^\.]+$/.mp3/')
+    dst="${DSTDIR}/${dir}/${mp3}"
+    mkdir -p "${DSTDIR}/${dir}"
+    echo "${dst}"
+    /opt/ffmpeg/bin/ffmpeg -y -i "${origFile}" -map 0:a -c:a libmp3lame -q:a 3 -id3v2_version 3 "${dst}" < /dev/null
+done
